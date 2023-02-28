3489f276cdab544fbd9408d49b576b5ac0aa08b2
Muhammad Noman <muhammad.noman@ceative.co.uk>
2023-02-28 14:46:09 +0500

    Worked on Global Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { Space } from 'antd'
-import { Button } from './components'
 
+import BoxWrapper from './components/BoxWrapper/BoxWrapper'
+import GlobalTable from './components/Table/Table'
 function App() {
-
   return (
     <div className="p-10">
-      <Space>
-        <Button type='dashed' label='new' />
-        <Button type='primary' label='stuff' />
-      </Space>
+      <BoxWrapper>
+        <GlobalTable/>
+      </BoxWrapper>
     </div>
   )
 }
