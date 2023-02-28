0e986afcbce2acb149bb88e5e52c57aa9ba36c55
Jawad <jawad.sadiq@ceative.co.uk>
2023-02-28 14:21:57 +0500

    theme color added by using atnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { Space } from 'antd'
+import { Space, Typography } from 'antd'
 import { Button } from './components'
 
 function App() {
 
   return (
     <div className="p-10">
-      <Space>
-        <Button type='dashed' label='new' />
-        <Button type='primary' label='stuff' />
-      </Space>
+      <Button type='dashed' label='new' />
+      <Button type='primary' label='stuff' />
+      <br />
+      <Typography.Title level={1}>
+        I'm h1
+      </Typography.Title>
+
+      <br />
+
+      <Typography.Title level={2}>
+        I'm h2
+      </Typography.Title>
+
+      <br />
+
+      <Typography.Title level={3}>
+        I'm h3
+      </Typography.Title>
     </div>
   )
 }
