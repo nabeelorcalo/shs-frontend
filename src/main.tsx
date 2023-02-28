adaf20356909dafafd06c0fad93c787ea371a087
Jawad <jawad.sadiq@ceative.co.uk>
2023-02-28 14:48:28 +0500

    index file imported into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ConfigProvider } from 'antd'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.scss';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
