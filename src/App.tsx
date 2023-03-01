e134a2b0a23e9e12ad14d559daccfe724df3558d
Shayan ul haq <Shayan.ulhaq@ceative.co.uk>
2023-03-01 15:53:32 +0500

    add global scss classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React, {FC, useEffect} from 'react'
+import React, { FC, useEffect } from 'react'
+import Charts from './components/ChartsOfGraphs/Chats'
 import AppLayout from './layout'
+import "../src/App.scss";
 
 function App() {
   /* VARIABLE DECLARATION
@@ -17,13 +19,16 @@ function App() {
 
   /* EVENT FUNCTIONS
   -------------------------------------------------------------------------------------*/
-  
+
 
 
   /* RENDER APP
   -------------------------------------------------------------------------------------*/
   return (
-    <AppLayout />
+    <>
+      <AppLayout />
+      <Charts />
+    </>
   )
 }
 
