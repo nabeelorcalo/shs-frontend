5972ac403c2323d2f393a766fe45beeec3c3d016
Muhammad Noman <muhammad.noman@ceative.co.uk>
2023-03-02 13:29:58 +0500

    Worked on Table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,10 +11,10 @@ interface TableProps {
     expandIcon?: any;
 }
 const GlobalTable = (props: TableProps) => {
-    let { columns, tableData, pagination = true, bgWhiteTable  } = props
+    let { columns, tableData, pagination = true, bgWhiteTable, ...rest  } = props
     return (
         <div className={`${bgWhiteTable? "whiteHeadTable":"primary_table_wrapper"}`}>
-            <Table columns={columns} dataSource={tableData} pagination={pagination} scroll={{ x: 'scroll' }}/>
+            <Table columns={columns} dataSource={tableData} pagination={pagination} scroll={{ x: 'scroll' }} {...rest}/>
             {pagination && <span className='Counter'> Total:{tableData.length}</span>}
         </div>
 
