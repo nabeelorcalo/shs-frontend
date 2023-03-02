66b748e298f5035a8fdc76b0e53451d8e30eb4f6
Jawad <jawad.sadiq@ceative.co.uk>
2023-03-02 14:51:05 +0500

    registered member graph - initital commit

---
+++ src/pages/dummy/data.ts
@@ -0,0 +1,39 @@
+const data = [
+  { "month": "Jan", "area": "华东", "value": 238477 },
+  { "month": "Jan", "area": "中南", "value": 97700 },
+
+  { "month": "Feb", "area": "东北", "value": 77127 },
+  { "month": "Feb", "area": "华北", "value": 192508 },
+
+  { "month": "Mar", "area": "东北", "value": 171283 },
+  { "month": "Mar", "area": "华北", "value": 140033 },
+
+  { "month": "Apr", "area": "东北", "value": 353119 },
+  { "month": "Apr", "area": "华北", "value": 262622 },
+
+  { "month": "May", "area": "东北", "value": 211428 },
+  { "month": "May", "area": "华北", "value": 60705 },
+
+  { "month": "Jun", "area": "东北", "value": 101513 },
+  { "month": "Jun", "area": "华北", "value": 119662 },
+
+  { "month": "Jul", "area": "东北", "value": 159517 },
+  { "month": "Jul", "area": "华北", "value": 118773 },
+
+  { "month": "Aug", "area": "东北", "value": 272273 },
+  { "month": "Aug", "area": "华北", "value": 225966 },
+
+  { "month": "Sep", "area": "东北", "value": 190567 },
+  { "month": "Sep", "area": "华北", "value": 80122 },
+
+  { "month": "Oct", "area": "东北", "value": 161600 },
+  { "month": "Oct", "area": "华北", "value": 291271 },
+
+  { "month": "Nov", "area": "东北", "value": 267974 },
+  { "month": "Nov", "area": "华北", "value": 206705 },
+
+  { "month": "Dec", "area": "东北", "value": 237980 },
+  { "month": "Dec", "area": "华北", "value": 216705 },
+];
+
+export default data;
--- src/pages/dummy/index.tsx
+++ src/pages/dummy/index.tsx
@@ -1,10 +1,99 @@
-import { useState } from "react";
-import "./style.scss";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { Mix, G2 } from '@ant-design/plots';
+import gdata from './data';
 
 const Graph = () => {
-  return (
-    <p>Graph</p>
-  )
-}
+  const [data, setData] = useState(gdata);
+
+  useEffect(() => {
+  }, []);
+
+  G2.registerInteraction('custom-association-filter', {
+    showEnable: [
+      {
+        trigger: 'element:mouseenter',
+        action: 'cursor:pointer',
+      },
+      {
+        trigger: 'element:mouseleave',
+        action: 'cursor:default',
+      },
+    ],
+    start: [
+      {
+        trigger: 'element:click',
+        action: (context: any) => {
+          const { view, event } = context; // 获取第二个 view
+          const view1 = view.parent.views[1];
+          view1.filter('area', (d: any) => d === event.data?.data.area);
+          view1.render(true);
+        },
+      },
+    ],
+    end: [
+      {
+        trigger: 'element:dblclick',
+        action: (context: any) => {
+          const { view } = context; // 获取第二个 view
+
+          const view1 = view.parent.views[1];
+          view1.filter('area', null);
+          view1.render(true);
+        },
+      },
+    ],
+  });
+  if (!Object.keys(data).length) {
+    return null;
+  }
+  const config = {
+    tooltip: false,
+    plots: [
+      {
+        type: 'area',
+        yAxis: {
+          // other yAxis options
+          formatter: (value: any) => `${(value * 100).toFixed(0)}%`,
+        },
+        region: {
+          start: {
+            x: 0,
+            y: 0.5,
+          },
+          end: {
+            x: 1,
+            y: 0.95,
+          },
+        },
+        options: {
+          data: data,
+          xField: 'month',
+          yField: 'value',
+          seriesField: 'area',
+          line: {},
+          point: {
+            style: {
+              r: 2.5,
+            },
+          },
+          meta: {
+            month: {
+              range: [0, 1],
+            },
+          },
+          smooth: true,
+          tooltip: {
+            showCrosshairs: true,
+            shared: true,
+          },
+        },
+      },
+    ],
+  };
+
+  return <Mix {...config} />;
+};
+
 
 export default Graph
