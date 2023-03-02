0179e6336040681bdb70e795af8cb903c97d0e51
Jawad <jawad.sadiq@ceative.co.uk>
2023-03-02 08:59:13 +0500

    routing apply in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
-import React, {FC, useEffect} from 'react'
+import React, { FC, useEffect } from 'react'
+import { useLocation, useNavigate, useRoutes } from "react-router-dom";
+import { getRoutes } from "./routes";
+import "./App.css";
+// import { ErrorBoundary } from "react-error-boundary";
 import AppLayout from './layout'
 
 function App() {
   /* VARIABLE DECLARATION
   -------------------------------------------------------------------------------------*/
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const userData: any = JSON.parse(localStorage.getItem("UserData") || "{}");
+  const routes = getRoutes(userData.role);
+  const pages = useRoutes(routes);
 
-
-
-  /* EVENT LISTENERS
+  /* HOOKS
   -------------------------------------------------------------------------------------*/
   useEffect(() => {
-
-  }, [])
-
-
+    if (
+      !userData.token &&
+      !pathname.includes("sign-up") &&
+      !pathname.includes("forget-password") &&
+      !pathname.includes("reset-password")
+    ) {
+      navigate("/login");
+    }
+  }, [pathname]);
 
   /* EVENT FUNCTIONS
   -------------------------------------------------------------------------------------*/
-  
+
 
 
   /* RENDER APP
   -------------------------------------------------------------------------------------*/
   return (
-    <AppLayout />
+    // <ErrorBoundary FallbackComponent={ErrorFallback}>
+      {pages}
+    // </ErrorBoundary>
   )
 }
 
