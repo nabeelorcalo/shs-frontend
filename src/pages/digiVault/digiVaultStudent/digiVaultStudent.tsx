b8384381b1c30a068be32bd0a82304b914317f68
Abubakar Saddiq <abubakar.saddiq@ceative.co.uk>
2023-03-03 10:03:58 +0500

    working on digiVault design

---
+++ src/pages/digiVault/digiVaultStudent/digiVaultStudent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const digiVaultStudent = () => {
+  return <div>digiVaultStudent</div>;
+};
+
+export default digiVaultStudent;
--- src/pages/digiVault/index.tsx
+++ src/pages/digiVault/index.tsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 import "./style.scss";
 
 const DigiVault = () => {
-  return (
-    <p>DigiVault</p>
-  )
-}
+  return <p>feewfewfewfewfwefewf</p>;
+};
 
-export default DigiVault
+export default DigiVault;
