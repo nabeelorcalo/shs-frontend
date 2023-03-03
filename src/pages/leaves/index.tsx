286590970f0645d549e9006103e91ff2c01e587f
Muhammad Noman <muhammad.noman@ceative.co.uk>
2023-03-03 12:47:14 +0500

    Structure created

--- src/pages/leaves/index.tsx
+++ src/pages/leaves/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./style.scss";
+import Internee from "./interne/index"
 
 const Leaves = () => {
   return (
-    <p>Leaves</p>
+    <p><Internee/></p>
   )
 }
 
---
+++ src/pages/leaves/interne/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>Intrnee</div>
+  )
+}
+
+export default index
