6376596153115855806309b960d45e8b55ec5665
ayaz.khan <ayaz.khan@ceative.co.uk>
2023-03-07 14:11:36 +0500

    add request message and set a goal added

--- src/components/CreateFolderModal/index.tsx
+++ src/components/CreateFolderModal/index.tsx
@@ -23,7 +23,7 @@ const CreateFolderModal = ({ title }: any) => {
               Cancel
             </Button>,
             <Button onClick={() => { setShow(!show) }} key="submit" style={{ backgroundColor: '#4a9d77', color: '#fff', border: '1px solid #4a9d77', padding: '0px 20px' }}>
-              Submit
+              Create
             </Button>,
           ]}
         >
--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -7,12 +7,25 @@ import AssesmentModal from "../../components/AssesmentModal"
 import MyProfileDocUpload from "../../components/MyProfileDocUpload";
 import CreateFolderModal from "../../components/CreateFolderModal";
 import EditGoalTask from "../../components/EditGoalTask";
+import AddRequestMessage from "../../components/AddRequestMessage";
+import SetaGoal from "../../components/SetaGoal";
 
 const Calendar = () => {
   const name = "Calvin Grayson"
   return (
-
+    <>
       <p>Calendar </p>
+      <div className="flex gap-3">
+        <LeaveRequest title="Leave Request" />
+        <EmojiEvaluation title={`Performance Report - ${name}`} />
+        <AssesmentModal title="Signature" />
+        <MyProfileDocUpload title="Upload Documents" />
+        <CreateFolderModal title="Create New Folder" />
+        <EditGoalTask title="Edit Goal Task" />
+        <AddRequestMessage  title="Add Request Message" />
+        <SetaGoal title="Set a Goal"/>
+      </div>
+    </>
 
   )
 }
