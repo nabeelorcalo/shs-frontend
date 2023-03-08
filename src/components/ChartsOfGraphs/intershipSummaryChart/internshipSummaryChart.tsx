70f757caabb639fcc313cad02fedda5b5194cb52
Shayan malik <shayan.ulhaq@ceative.co.uk>
2023-03-08 05:59:49 +0000

    Squashed commit of the following
    commit e5938f24 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Wed Mar 08 2023 10:38:50 GMT+0500 (Pakistan Standard Time) 
    
        #comment merged with dev
    
    
    commit 1c42cb1a 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Wed Mar 08 2023 10:30:27 GMT+0500 (Pakistan Standard Time) 
    
        #comment merging with dev
    
    
    commit a106ed4e 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Wed Mar 08 2023 08:33:06 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev' into shayan-malik
    
    
    commit 9b80ea7c 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Tue Mar 07 2023 14:59:32 GMT+0500 (Pakistan Standard Time) 
    
        #stories added
    
    
    commit 66d70ffd 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Tue Mar 07 2023 11:56:06 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev' into shayan-malik
    
    
    commit 23bc38f4 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Tue Mar 07 2023 11:55:08 GMT+0500 (Pakistan Standard Time) 
    
        #comment merging with dev
    
    
    commit 8176a093 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Mon Mar 06 2023 11:00:13 GMT+0500 (Pakistan Standard Time) 
    
        #merging with dev
    
    
    commit 6c127dcc 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Thu Mar 02 2023 19:17:55 GMT+0500 (Pakistan Standard Time) 
    
        #new changing

---
+++ src/components/ChartsOfGraphs/intershipSummaryChart/internshipSummaryChart.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { InterShipData } from "./internShipData";
+import { RadialBar } from "@ant-design/plots";
+import {BoxWrapper} from "../../BoxWrapper/boxWrapper";
+
+export const InternshipSummaryChart = (props: any) => {
+    const {
+        xField = "name",
+        yField = "star",
+        autoFit = true,
+        padding = "auto",
+        maxAngle = 360,
+        radius = 0.8,
+        innerRadius = 0.37,
+        colorField = "name",
+        barStyle = { lineCap: "round", },
+        intervalPadding = 9.1,
+        xAxis = { label: null },
+        heading,
+    } = props
+
+    const [data] = useState(InterShipData);
+
+    const config: any = {
+        data,
+        xField: xField,
+        yField: yField,
+        autoFit: autoFit,
+        padding: padding,
+        maxAngle: maxAngle,
+        radius: radius,
+        innerRadius: innerRadius,
+        colorField: colorField,
+        barBackground: {},
+        color: ({ name }: any) => {
+            if (name == "Gray") {
+                return "#A0A3BD";
+            } else if (name == "yellow") {
+                return "#FFC15E";
+            } else if (name == "blue") {
+                return "#4783FF";
+            } else if (name == "green") {
+                return " #4A9D77";
+            }
+            return "#ff93a7";
+        },
+        barStyle: barStyle,
+        intervalPadding: intervalPadding,
+        xAxis: xAxis,
+    };
+    return (
+        <BoxWrapper>
+            {heading && <p className="text-secondary-color font-medium text-xl">{heading}</p>}
+            <RadialBar
+                style={{ height: "300px", marginTop: "-15px" }}
+                tooltip={false}
+                {...config}
+                className="transition-from-right"
+            />
+        </BoxWrapper>
+    )
+}
