4e6805a69c5e2191db0eade453925ae04a36558d
hamza.akhtar <hamza.akhtar@ceative.co.uk>
2023-03-09 14:01:17 +0500

    change password

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,10 +2,14 @@ import { Col, Divider, Row, Typography } from "antd";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import StudentsTabs from "./students";
+import AddVideo from "./students/addVideo";
+import ChangePassword from "./students/changePassword";
 import StudentSideBar from "./studentSideBar";
 import "./style.scss";
 
 const Profile = () => {
+  const [showSideViewType, setShowSideViewType] = useState("student-tabs");
+
   return (
     <div className="main-student-profile">
       <Row gutter={10}>
@@ -17,10 +21,13 @@ const Profile = () => {
         </Col>
 
         <Col xxl={6} xl={6} lg={10} md={24} sm={24} xs={24}>
-          <StudentSideBar />
+          <StudentSideBar setShowSideViewType={setShowSideViewType} />
         </Col>
         <Col xxl={18} xl={18} lg={14} md={24} sm={24} xs={24}>
-          <StudentsTabs/>
+          {showSideViewType === "add-video" && <AddVideo />}
+
+          {showSideViewType === "student-tabs" && <StudentsTabs />}
+          {showSideViewType === "change-password" && <ChangePassword />}
         </Col>
       </Row>
     </div>
