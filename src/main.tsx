1714bb87d51e4f840ff6ab7dc30c034af913b243
Haseeb Ahmed <engg.haseeb@gmail.com>
2023-03-12 16:17:23 +0500

    store by using recoil implementation

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import { ConfigProvider } from "antd";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.scss";
 import { theme } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { getRoutes } from "./routes";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
