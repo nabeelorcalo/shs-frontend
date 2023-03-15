fb3816191d0a6ee1d8cd9cb61070614a6a7668a1
Ghulam Mohiuddin <ghulam.mohiuddin@ceative.co.uk>
2023-03-15 11:42:47 +0500

    Updated Modal reusable component

--- src/pages/accommodation/PropertyDetail/BookingRequest/index.tsx
+++ src/pages/accommodation/PropertyDetail/BookingRequest/index.tsx
@@ -1,13 +1,15 @@
-import React, { useState, FC } from "react";
-import './style.scss'
-import type { DatePickerProps } from 'antd';
-import { Form, Button, Col, Row, Popover, Checkbox  } from 'antd'
+import React, { useState, FC } from "react"
+import { Link } from 'react-router-dom'
+import type { DatePickerProps } from 'antd'
+import { Form, Button, Col, Row, Popover, Checkbox, Modal, Typography  } from 'antd'
 import { SaveIcon, IconInfoCircle } from '../../../../assets/images'
-import { DatePicker } from "../../../../components";
+import { DatePicker } from "../../../../components"
+import './style.scss'
 
 
 const PropertyPricing = () => {
   const [checkAvailability, setCheckAvailability] = useState(false)
+  const [modalDisclaimerOpen, setModalDisclaimerOpen] = useState(false)
 
 
   const onFinish = (values: any) => {
@@ -22,86 +24,118 @@ const PropertyPricing = () => {
     setCheckAvailability(true)
   }
 
+  const openModalDisclaimer = () => {
+    setModalDisclaimerOpen(true)
+  }
+
+  const closeModalDisclaimer = () => {
+    setModalDisclaimerOpen(false)
+  }
+
   return (
-    <div className="card-booking-request">
-      <div className="booking-request-header">
-        <div className="booking-request-header-title">£700 / <span>month</span></div>
-        <div className="request-available-from">
-          <div className="available-from-text">
-            Available From: <span>9 February</span>
-          </div>
-          <div className="save-property-button">
-            <SaveIcon />
+    <>
+      <div className="card-booking-request">
+        <div className="booking-request-header">
+          <div className="booking-request-header-title">£700 / <span>month</span></div>
+          <div className="request-available-from">
+            <div className="available-from-text">
+              Available From: <span>9 February</span>
+            </div>
+            <div className="save-property-button">
+              <SaveIcon />
+            </div>
           </div>
         </div>
-      </div>
 
-      <Form layout="vertical" name="bookingRequest" onFinish={onFinish}>
-        <Row gutter={20}>
-          <Col xs={12}>
-            <Form.Item name="moveInDate" label="Move-in Date">
-              <DatePicker 
-                onChange={onChange}
-              />
-            </Form.Item>
-          </Col>
-          <Col xs={12}>
-            <Form.Item name="moveOutDate" label="Move-out Date">
-              <DatePicker 
-                onChange={onChange}
-              />
-            </Form.Item>
-          </Col>
-          <Col xs={24}>
-            <div className="booking-request-general-info">
-              <div className="general-info-message">
-                <span>Total to pay per booking request</span>
-                {checkAvailability ? (
-                  <div className="disclaimer-modal">
-                    <IconInfoCircle />
-                  </div>
-                ) : (
-                  <div className="general-info-popover">
-                    <Popover 
-                      trigger="hover"
-                      content={`You will only be charged once the booking is accepted. As an extra security step,  we'll send the money to the Landlord after contract is signed.`}
-                    >
+        <Form layout="vertical" name="bookingRequest" onFinish={onFinish}>
+          <Row gutter={20}>
+            <Col xs={12}>
+              <Form.Item name="moveInDate" label="Move-in Date">
+                <DatePicker 
+                  onChange={onChange}
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={12}>
+              <Form.Item name="moveOutDate" label="Move-out Date">
+                <DatePicker 
+                  onChange={onChange}
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={24}>
+              <div className="booking-request-general-info">
+                <div className="general-info-message">
+                  <span>Total to pay per booking request</span>
+                  {checkAvailability ? (
+                    <div className="disclaimer-modal" onClick={openModalDisclaimer}>
                       <IconInfoCircle />
-                    </Popover>
+                    </div>
+                  ) : (
+                    <div className="general-info-popover">
+                      <Popover
+                        content={`You will only be charged once the booking is accepted. As an extra security step,  we'll send the money to the Landlord after contract is signed.`}
+                      >
+                        <IconInfoCircle />
+                      </Popover>
+                    </div>
+                  )}
+                </div>
+                
+                {checkAvailability ? (
+                  <Form.Item>
+                    <Checkbox>
+                      I accept that I have read and understand the information given in <Link to="">disclaimer</Link> and <Link to="">cancelation policy</Link> .
+                    </Checkbox>
+                  </Form.Item>
+                ) : (
+                  <div className="general-info-text">
+                    Includes one month rent in advance and the Student help squad fee.
                   </div>
                 )}
               </div>
-              <div className="general-info-text">
-                Includes one month rent in advance and the Student help squad fee.
-              </div>
-            </div>
-          </Col>
-          {checkAvailability &&
-            <Col xs={24}>
-              <Form.Item>
-                <Checkbox>
-                  I have read the <a href="">agreement</a>
-                </Checkbox>
-              </Form.Item>
             </Col>
-          }
-          
-          <Col xs={24}>
-            {checkAvailability ? (
-              <Form.Item>
-                <Button type="primary" block htmlType="submit">
-                  Send Booking Request
+            <Col xs={24}>
+              {checkAvailability ? (
+                <Form.Item>
+                  <Button type="primary" block htmlType="submit">
+                    Send Booking Request
+                  </Button>
+                </Form.Item>
+              ) : (
+                <Button type="primary" block onClick={handleCheckAvailability}>
+                  Check Availibility
                 </Button>
-              </Form.Item>
-            ) : (
-              <Button type="primary" block onClick={handleCheckAvailability}>
-                Check Availibility
-              </Button>
-            )}
-          </Col>
-        </Row>
-      </Form>
-    </div>
+              )}
+            </Col>
+          </Row>
+        </Form>
+      </div>
+
+      {/* MODAL DISCALIMER */}
+      <Modal
+        open={modalDisclaimerOpen}
+        onCancel={closeModalDisclaimer}
+        centered
+        closable={false}
+        footer={null}
+        width={510}
+        wrapClassName={'disclaimer-modal-container'}
+      >
+        <div className="disclaimer-content">
+          <Typography.Title level={5}>Disclaimer</Typography.Title>
+          <Typography.Text>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+          </Typography.Text>
+        </div>
+        <div className="cancellation-policy">
+          <Typography.Title level={5}>Cancellation Policy</Typography.Title>
+          <Typography.Text>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+          </Typography.Text>
+        </div>
+      </Modal>
+    </>
   )
 }
 
