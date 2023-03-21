9e23a9301864142652607411049fc5db1dbf5d10
arsalan <arsalan.tahir@ceative.co.uk>
2023-03-21 18:58:50 +0500

    #comment

--- src/components/calendars/FullCalendarComp/index.tsx
+++ src/components/calendars/FullCalendarComp/index.tsx
@@ -7,6 +7,7 @@ import { PageHeader } from "../../PageHeader";
 import dayjs from "dayjs";
 import { BoxWrapper } from "../../BoxWrapper/BoxWrapper";
 import './style.scss';
+import { Button } from "antd";
 
 const Index = () => {
   const events = [
@@ -14,29 +15,63 @@ const Index = () => {
       id: "1",
       title: "Post UI UX Meeting",
       start: "2023-03-21T01:51:00",
-      end: "2023-03-21T01:51:00",
+      end: "2023-03-21T02:51:00",
+      category: "meeting",
     },
     {
       id: "2",
+      title: "UI UX post oppertunity",
+      start: "2023-03-21T03:00:00",
+      end: "2023-03-21T04:05:00",
+      category: "interview"
+    },
+    {
+      id: "3",
+      title: "annual fee submission",
+      start: "2023-03-24T03:51:00",
+      end: "2023-03-24T06:51:00",
+      category: "event"
+    },
+    {
+      id: "4",
       title: "Post UI UX Meeting",
-      start: "2023-03-23T01:51:00",
-      end: "2023-03-23T01:51:00",
+      start: "2023-03-25T01:51:00",
+      end: "2023-03-25T01:51:00",
     }
   ];
 
+  const renderEventColor: any = {
+    'meeting': '#E94E5D',
+    'interview': '#5879CE',
+    'event': '#FFC15D',
+  }
+
   const handleEventContent = (info: any) => {
-    const events = info?.event?._def
-    console.log("info", info)
+    const events = info?.event?._def;
+    const { start, end } = info?.event?._instance?.range;
+    const category = events?.extendedProps?.category;
     return (
-      <div className="event-content bg-white text-[#14142A] w-full h-full px-5 py-4">
-        <h2 className="text-[14px] font-normal m-0">{events?.title}</h2>
-        <p className="text-[14px] text-[#4E4B66] m-0">09:00 - 10:00</p>
+      <div className="event-content"
+        style={{ borderLeft: `2px solid ${renderEventColor[category] ? renderEventColor[category] : '#4E4B66'}` }}
+      >
+        <h2 className="title text-[14px] capitalize break-words font-normal m-0">{events?.title}</h2>
+        <p className="duration text-[14px] mt-[5px]">{dayjs(start).format('HH:MM')} - {dayjs(end).format('HH:MM')}</p>
+        <div className="event-btn gap-3">
+          <Button size="small" className={`btn capitalize btn-primary`}>
+            edit
+          </Button>
+
+          <Button size="small" className={`btn capitalize`}>
+            cancel
+          </Button>
+        </div>
       </div>
     )
   }
 
   return (
     <div className='role-calendar-wrapper'>
+
       <PageHeader title={'Calendar'} bordered />
 
       <BoxWrapper boxShadow="0px 0px 8px 2px rgba(9, 161, 218, 0.1)" className="rounded-[10px]">
@@ -63,13 +98,6 @@ const Index = () => {
           }}
         />
       </BoxWrapper>
-
-      {/* // slotLabelFormat={[{ day: "2-digit", month: "long", year: "numeric", weekday: "long" }]}
-      // slotLabelContent={(conternt: any) => <div>gfgfgfg</div>}
-      // events={newEventsArray}
-      // eventContent={eventContentHandler}
-      // eventClick={(e: any) => handleEventClick(e)}  */}
-      {/* /> */}
     </div>
   )
 }
