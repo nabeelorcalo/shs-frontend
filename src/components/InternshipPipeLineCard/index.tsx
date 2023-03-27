c8681bc4a8b1fc1221a2c632d3b4fa938f4154af
ayaz.khan <ayaz.khan@ceative.co.uk>
2023-03-27 11:05:41 +0500

    pipeline card image avatar re added

--- src/components/InternshipPipeLineCard/index.tsx
+++ src/components/InternshipPipeLineCard/index.tsx
@@ -5,7 +5,7 @@ export const InternshipPipeLineCard = () => {
   return (
     <div className='border-2 border-solid border-[#e2e2e2] rounded-lg p-2'>
       <div className='flex flex-row items-center gap-3'>
-        <Avatar src={<img src={`https://joesch.moe/api/v1/random?key=${Math.floor(Math.random() * 10)}`} alt="avatar" />} />
+        <Avatar src={<img src={`https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png`} alt="avatar" />} />
         <div className='flex flex-col gap-2 justify-center'>
           <p>Jane Cooper</p>
           <p className='p-1 bg-[#f3f1e3] rounded-lg'>Pending</p>
