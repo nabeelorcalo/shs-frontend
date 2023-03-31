27fafd0a0860029552921a80c28016b2f807a124
shahzebrehmankhattak <shahzebkhattak703@gmail.com>
2023-03-31 16:48:42 +0500

    workingonslider

--- src/pages/dashboard/student/mainDataSide/CardStatic.tsx
+++ src/pages/dashboard/student/mainDataSide/CardStatic.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Row, Col, Typography } from 'antd';
-import { Apply, InterView, InterviewIcon, Sun } from '../../../../assets/images';
+import { Apply, InterView, LocationSun, Sun } from '../../../../assets/images';
 
 const CardStatic = () => {
     return (
         <>
               <Row gutter={[15,15]}>
     <Col xxl={8} xl={10} lg={12} md={12} sm={24} xs={24}>
-                    <div className='card-static '>
-                       
+                    <div className='card-static '>   
                         <div className='card-upper'>
                             <div className='one'>
-                            <Apply style={{height:"70px",width:"150px"}}/>
+                            <Apply style={{height:"100px",width:"100px"}}/>
                         </div>
                             <Apply />
                             <div>
-                            <Typography>Apply</Typography>
-                        <Typography>06</Typography>
+                            <Typography className='card-head'>Apply</Typography>
+                        <Typography className='card-number'>06</Typography>
                             </div>
                       
                         </div> 
@@ -34,8 +33,8 @@ const CardStatic = () => {
                         </div>
                             <InterView />
                             <div>
-                            <Typography>Apply</Typography>
-                        <Typography>06</Typography>
+                            <Typography className='card-head'>Interviews</Typography>
+                        <Typography className='card-number'>03</Typography>
                             </div>
                       
                         </div> 
@@ -46,8 +45,13 @@ const CardStatic = () => {
                         <div className='flex items-center gap-x-6'>
                             
                             <Sun />
-                            <Typography>23 ° C</Typography>
+                            <Typography className='temp'>23 ° C</Typography>
             </div>
+                        <div>
+                            <Typography className='date-sun'>Monday, 21 September</Typography>
+                            <Typography className='location'><LocationSun/> London</Typography>
+                        </div>
+                        
         </div>
     </Col>
 </Row>
--- src/pages/dashboard/student/mainDataSide/index.tsx
+++ src/pages/dashboard/student/mainDataSide/index.tsx
@@ -1,12 +1,58 @@
 import React from 'react'
-import { Row, Col } from 'antd';
+import { Row, Col, Typography, Avatar } from 'antd';
 import CardStatic from './CardStatic';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const data = [
+  { id: '001', avatar: 1, title: 'Austin Wade' },
+  { id: '002', avatar: 2, title: 'Amelia Clark' },
+  { id: '003', avatar: 3, title: 'Christopher Campbell' },
+  { id: '004', avatar: 4, title: 'David Miller' },
+  { id: '005', avatar: 5, title: 'Austin Wade' },
+  { id: '006', avatar: 6, title: 'Amelia Clark' },
+  { id: '007', avatar: 7, title: 'Austin Wade' },
+  { id: '008', avatar: 8, title: 'Christopher Campbell' },
+  { id: '009', avatar:9, title: 'Austin Wade' },
+]
 
 const MainDataSide = () => {
+  const settings = {
+    arrows: false,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    variableWidth: true,
+    speed: 1200,
+    slidesToScroll: 1,
+  };
   return (
       <div className='main-data-side'> 
           <div className='card-style'>
-            <CardStatic/>
+        <CardStatic />
+        <div className='pt-3'>
+          <Typography className='main-title'>Recent Jobs</Typography>
+          <Slider {...settings}>
+          {data.map((item) => {
+                return (
+                  <div key={item.id} className="carousel-item">
+                    <div className="carousel-card">
+                      <div className="carousel-card-avatar">
+                        <Avatar src={item.avatar} size={48} />
+                      </div>
+                      <div className="carousel-card-title">{item.title}</div>
+                      <div className="carousel-card-rate">
+                        {/* <Rate value={3} /> */}
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+    </Slider>
+        </div>
+
           </div>
     </div>
   )
