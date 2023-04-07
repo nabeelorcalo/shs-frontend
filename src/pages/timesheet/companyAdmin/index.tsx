b2b04ef5846abf167983ba9c014e7834eca62573
arsalan <arsalan.tahir@ceative.co.uk>
2023-04-07 12:02:38 +0500

    #comment

--- src/pages/timesheet/companyAdmin/index.tsx
+++ src/pages/timesheet/companyAdmin/index.tsx
@@ -5,40 +5,42 @@ import { timesheetMock } from "../mockData";
 import "./style.scss";
 import { PageHeader } from "../../../components";
 import { useState } from "react";
-import { downloadPDF } from "../../../components/pdfExcelDownload";
 import { ROUTES_CONSTANTS } from "../../../config/constants";
+import useCustomHook from "../actionHandler";
 
 const CompanyAdmin = () => {
+  const action = useCustomHook();
   const navigate = useNavigate();
   const [download, setDownload] = useState('');
 
-  const handleDownload = (val: string) => {
-    setDownload(val);
-    downloadPDF('timesheet-pdf-download');
-  }
+  const PdfHeader = ['No', 'User Name', 'Designation', 'Total Hours', 'Progress', 'Worked Hours'];
+
+  const PdfBody = timesheetMock.map(({ id, userName, designation, totalHours, progess, workedHours }: any) =>
+    [id, userName, designation, totalHours, `${progess}%`, workedHours]
+  );
 
   return (
     <div className="timesheet-wrapper">
       <PageHeader title='Timesheet' bordered />
 
-      <CommonHeader download={download} setDownload={handleDownload} />
+      <CommonHeader download={download}
+        setDownload={() => action.downloadPdfOrCsv(event, PdfHeader, timesheetMock, 'Timesheet-History', PdfBody)}
+      />
 
-      <div id="timesheet-pdf-download">
-        {timesheetMock.map((data, i) => (
-          <ContractCard key={i}
-            className='mt-[30px] timesheet-work-history'
-            cardWithProgressBar
-            userName={data.userName}
-            designation={data.designation}
-            userImg={data.userImg}
-            progress={data.progess}
-            strokeColor={'#3DC575'}
-            totalHours={data.totalHours}
-            workedHours={data.workedHours}
-            handleViewAll={() => navigate(`/${ROUTES_CONSTANTS.TIMESHEETHISTORY}/${data.id}`)}
-          />
-        ))}
-      </div>
+      {timesheetMock.map((data, i) => (
+        <ContractCard key={i}
+          className='mt-[30px] timesheet-work-history'
+          cardWithProgressBar
+          userName={data.userName}
+          designation={data.designation}
+          userImg={data.userImg}
+          progress={data.progess}
+          strokeColor={'#3DC575'}
+          totalHours={data.totalHours}
+          workedHours={data.workedHours}
+          handleViewAll={() => navigate(`/${ROUTES_CONSTANTS.TIMESHEETHISTORY}/${data.id}`)}
+        />
+      ))}
 
     </div>
   )
--- src/pages/timesheet/companyAdmin/timesheetHistory.tsx
+++ src/pages/timesheet/companyAdmin/timesheetHistory.tsx
@@ -6,21 +6,30 @@ import './style.scss';
 import { Breadcrumb } from '../../../components';
 import { useState } from 'react';
 import { ROUTES_CONSTANTS } from '../../../config/constants';
+import useCustomHook from '../actionHandler';
 
 const TimeSheetHistory = () => {
-
+  const action = useCustomHook();
   const { id } = useParams();
   const [download, setDownload] = useState('');
   const findTimesheet = timesheetMock.find(timesheet => timesheet.id === id);
 
+  const PdfHeader = ['Date', 'Total Tasks', 'Total Time'];
+
+  const PdfBody = findTimesheet?.history?.map(({ id, userName, designation, totalHours, progess, workedHours }: any) =>
+    [id, userName, designation, totalHours, `${progess}%`, workedHours]
+  );
+
   return (
     <div className='timesheet-history'>
       <Breadcrumb breadCrumbData={[{ name: 'History' }, { name: 'Timesheet', onClickNavigateTo: `/${ROUTES_CONSTANTS.TIMESHEET}` }]} />
-      <CommonHeader download={download} setDownload={setDownload} />
+      <CommonHeader download={download}
+        setDownload={() =>
+          action.downloadPdfOrCsv(event, PdfHeader, findTimesheet?.history, 'Timesheet-Detail-History', PdfBody)}
+      />
 
       {findTimesheet?.history ? findTimesheet?.history.map((data) => (
         <CommonTableCollapsible
-          activeKey={download === 'pdf' ? data.id : ''}
           key={data.id}
           id={data.id}
           dateTime={data.dateTime}
