2014152c1fe49ff067f3824a98a3a193162c412a
Abubakar Saddiq <abubakar.saddiq@ceative.co.uk>
2023-04-10 03:46:28 +0000

    Squashed commit of the following
    commit dd2b1a24 
    Author: Abubakar Saddiq <abubakar.saddiq@ceative.co.uk> 
    Date: Mon Apr 10 2023 08:39:31 GMT+0500 (Pakistan Standard Time) 
    
        merge with dev
    
    
    commit ba83c7a8 
    Author: Abubakar Saddiq <abubakar.saddiq@ceative.co.uk> 
    Date: Mon Apr 10 2023 08:37:37 GMT+0500 (Pakistan Standard Time) 
    
        working on 404 page

--- src/pages/errors/404/index.tsx
+++ src/pages/errors/404/index.tsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "../style.scss";
+import { Page_404, Page404 } from "../../../assets/images"
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <p>404</p>;
+  const navigate = useNavigate()
+  const handlClick = () => {
+    navigate("/")
+  }
+
+  return <div className="error-page p-16">
+    <center>
+      {/* <Page404 /> */}
+      <img src={Page_404} alt="404" />
+      <p className="text-3xl lg:text-5xl font-medium primary-color pb-16 pt-2">Page Not Found</p>
+      <div className="">
+        <Button className="font-semibold text-[16px] text-secondary-color bg-[#F1EFF2]" onClick={handlClick}>Back to Login</Button>
+      </div>
+    </center>
+  </div>
 };
 
 export default NotFound;
