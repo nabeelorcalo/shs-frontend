e84bdf8434754d6eb646a848f045b775a51a02a6
Muhammad Noman <muhammad.noman@ceative.co.uk>
2023-04-11 13:32:25 +0500

    Changes in rolbase component show

--- src/pages/dreamUp/index.tsx
+++ src/pages/dreamUp/index.tsx
@@ -1,24 +1,18 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
 import constants from "../../config/constants";
 import Internee from "./Intrnee";
 import Student from "./Student";
 import "./style.scss";
-import { useRecoilState } from "recoil";
-import { currentUserState } from "../../store";
+import {useRecoilValue } from "recoil";
+import { currentUserRoleState } from "../../store";
 
 const index = () => {
-  const [role, setRole] = useRecoilState(currentUserState);
-  console.log(role, 'role');
-  const rederWthRole: any = {
-    'INTERN': <Internee />,
-    'STUDENT': <Student />,
+  const { INTERN,STUDENT } = constants;
+  const role: string = useRecoilValue(currentUserRoleState)
+  switch (role) {
+    case STUDENT: return <Student />
+    case INTERN: return <Internee />
+    default: return <></>
   }
-  return (
-    <>
-      {rederWthRole[role]}
-    </>
-  )
 }
 
 export default index
