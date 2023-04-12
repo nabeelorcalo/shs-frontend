d0028e5ee4fbf5732897d599e7d30aa41bb5cf62
shahzeb rehman <shahzebrehman.khattak@ceative.co.uk>
2023-04-12 04:38:52 +0000

    Squashed commit of the following
    commit 79ee346f 
    Author: developer_shahzeb.rehman <shahzebrehman.khattak@ceative.co.uk> 
    Date: Wed Apr 12 2023 09:35:35 GMT+0500 (Pakistan Standard Time) 
    
        changedone
    
    
    commit 3c6b207d 
    Author: developer_shahzeb.rehman <shahzebrehman.khattak@ceative.co.uk> 
    Date: Wed Apr 12 2023 08:05:22 GMT+0500 (Pakistan Standard Time) 
    
        changesinmanager
    
    
    commit 7277e6ef 
    Author: developer_shahzeb.rehman <shahzebrehman.khattak@ceative.co.uk> 
    Date: Tue Apr 11 2023 08:18:08 GMT+0500 (Pakistan Standard Time) 
    
        404errorissuesolve

--- src/pages/managers/managerMain/managerInfo.tsx
+++ src/pages/managers/managerMain/managerInfo.tsx
@@ -1,25 +1,26 @@
-import { Button, Col, Row, Space, Typography } from "antd";
 import React from "react";
+import { Button, Col, Row, Space, Typography } from "antd";
 import { managerCar } from "./managerMock";
 
 const ManagerInfo = () => {
   return (
     <div className="manager-info">
-      <Row gutter={[10, 20]}>
+      <Row gutter={[15, 20]}>
         {managerCar.map((item, index) => {
           return (
             <>
-              <Col xxl={6} xl={6} lg={8} md={12} sm={24} xs={24}>
-                <div className="rounded-[10px] p-3 white-bg-color shadow-[0px 0px 8px 2px rgba(9, 161, 218, 0.1)] border-1 border-solid border-[#E6F4F9]">
+              <Col xxl={4} xl={6} lg={8} md={12} sm={24} xs={24}>
+                <div className="rounded-[10px] p-1  white-bg-color shadow-[0px 0px 8px 2px rgba(9, 161, 218, 0.1)] 
+                border-1 border-solid border-[#E6F4F9]">
                   <center>
-                    <img src={item.img} alt="" />
-                    <Typography className="text-2xl  text-primary-color font-medium">
+                    <img src={item.img} alt="" className="pt-3"/>
+                    <Typography className="text-2xl  text-primary-color font-medium pt-3">
                       {item.managerName}
                     </Typography>
-                    <Typography className="text-sm text-secondary-color font-normal">
+                    <Typography className="text-sm text-secondary-color font-normal pt-1 pb-3">
                       {item.desgination}
                     </Typography>
-                    <div className="border border-solid m-5 rounded-[8px]">
+                    <div className="border border-[#D9DBE9] border-solid ml-10 mr-10 mb-3 mt-3 rounded-[8px] p-3">
                       <Typography className="text-sm light-grey-color font-normal">
                         Assigned Interns
                       </Typography>
@@ -27,12 +28,14 @@ const ManagerInfo = () => {
                         {item.interns}
                       </Typography>
                     </div>
-                    <Space>
-                      <Button className="info-dark-bg-color  text-info-color-dark text-base font-semibold rounded-[8px]">
+                    <Space className="pb-3 pr-4 pl-3 pt-3">
+                      <Button style={{ minWidth: "0px" }} className="info-dark-bg-color text-info-color-dark text-base 
+                      font-semibold rounded-[8px] border-0 ">
                         <a href="managerprofile">Profile</a>
                         
                       </Button>
-                      <Button className="text-green-color reset-bg-color text-base font-semibold rounded-[8px]">
+                      <Button style={{ minWidth: "0px" }} className="text-green-color reset-bg-color text-base 
+                      ont-semibold rounded-[8px] border-0 ">
                         Reset
                       </Button>
                     </Space>
