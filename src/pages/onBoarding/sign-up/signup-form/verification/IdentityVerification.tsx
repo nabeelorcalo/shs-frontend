8d29aac39e4d5cd667e1793a3129c626b56333c8
developer_shahzeb.rehman <shahzebrehman.khattak@ceative.co.uk>
2023-04-12 10:52:50 +0500

    verisy email complete

--- src/pages/onBoarding/sign-up/signup-form/verification/IdentityVerification.tsx
+++ src/pages/onBoarding/sign-up/signup-form/verification/IdentityVerification.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Col,
@@ -7,11 +7,20 @@ import {
   Row,
   Select,
   Typography,
+  Modal
 } from "antd";
-import { BackButton,SHSLogo  } from "../../../../../assets/images";
+import { BackButton, IconCloseModal, SHSLogo } from "../../../../../assets/images";
+import step1 from '../../../../../assets/images/login/step1.svg';
+import step2 from '../../../../../assets/images/login/step2.svg';
+import step3 from '../../../../../assets/images/login/step3.svg';
 import "../../../styles.scss";
 
+
 const IdentityVerification = (props: any) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
   const { Option } = Select;
   const { currentStep, setCurrentStep } = props;
@@ -36,7 +45,7 @@ const IdentityVerification = (props: any) => {
               <div className="flex items-center mt-3 mb-3">
                 <div>
                   <BackButton
-                  //style={{ fill: '#363565', stroke: 'white', color: 'red' }}
+
                   />
                 </div>
                 <div className="mx-auto">
@@ -52,37 +61,37 @@ const IdentityVerification = (props: any) => {
               </Typography>
             </div>
             <div className="sign-up-form-wrapper">
-            
-                <Row gutter={20}>
-                  <Col xxl={12} xl={12} lg={12} md={12} sm={24} xs={24}>
-                    <Form.Item
-                      label="First Name"
-                      name="firstName"
-                      rules={[
-                        {
-                          required: true,
-                          message: "Please input your First Name!",
-                        },
-                      ]}
-                    >
-                      <Input placeholder="First Name" className="input-style" />
-                    </Form.Item>
-                  </Col>
-                  <Col xxl={12} xl={12} lg={12} md={12} sm={24} xs={24}>
-                    <Form.Item
-                      label="Last Name"
-                      name="lastName"
-                      rules={[
-                        {
-                          required: true,
-                          message: "Please input your Last Name!",
-                        },
-                      ]}
-                    >
-                      <Input placeholder="Last Name" className="input-style" />
-                    </Form.Item>
-                  </Col>
-                </Row>
+
+              <Row gutter={20}>
+                <Col xxl={12} xl={12} lg={12} md={12} sm={24} xs={24}>
+                  <Form.Item
+                    label="First Name"
+                    name="firstName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your First Name!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="First Name" className="input-style" />
+                  </Form.Item>
+                </Col>
+                <Col xxl={12} xl={12} lg={12} md={12} sm={24} xs={24}>
+                  <Form.Item
+                    label="Last Name"
+                    name="lastName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your Last Name!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Last Name" className="input-style" />
+                  </Form.Item>
+                </Col>
+              </Row>
 
               <Form.Item
                 label="Country"
@@ -106,41 +115,86 @@ const IdentityVerification = (props: any) => {
                 <Input placeholder="Document Type" className="input-style" />
               </Form.Item>
 
-                <Row gutter={[130,10]}>
-                  <Col xxl={4} xl={4} lg={5} md={24} sm={24} xs={24}>
+              <Row gutter={[130, 10]}>
+                <Col xxl={4} xl={4} lg={5} md={24} sm={24} xs={24}>
+                  <Button
+                    className="btn-cancel btn-cancel-verification"
+                  //htmlType="submit"
+                  >
+                    Skip
+                  </Button>
+                </Col>
+                <Col xxl={18} xl={18} lg={18} md={24} sm={24} xs={24}>
+                  <Form.Item>
                     <Button
-                      className="btn-cancel btn-cancel-verification"
+                      onClick={() => {
+                        console.log('hello')
+                        setCurrentStep(2);
+                      }}
+                      type="primary"
                       //htmlType="submit"
+                      className="login-form-button"
                     >
-                      Skip
+                      Next
                     </Button>
-                  </Col>
-                  <Col xxl={18} xl={18} lg={18} md={24} sm={24} xs={24}>
-                    <Form.Item>
-                      <Button
-                        onClick={() => {
-                          console.log('hello')
-                          setCurrentStep(2);
-                        }}
-                        type="primary"
-                         //htmlType="submit"
-                        className="login-form-button"
-                      >
-                        Next
-                      </Button>
-                    </Form.Item>
-                  </Col>
-                </Row>
+                  </Form.Item>
+                </Col>
+              </Row>
 
               <div>
-                <Typography className="text-center">
+                <Typography className="text-center cursor-pointer" onClick={showModal}>
                   Why I need to verify myself?
                 </Typography>
               </div>
             </div>
           </div>
         </Col>
+
       </Row>
+      <Modal centered width={700} closeIcon={<IconCloseModal onClick={() => {
+        setIsModalOpen(false)
+      }} />} open={isModalOpen} footer={null}>
+        <div className="verify-modal">
+          <Typography className="top-question">Why I need to verfiy myself?</Typography>
+          <Typography className="question-description">
+            Identity verification ensures that there is a real person behind a process
+            and proves that the one is who he or she claims to be, preventing both a
+            person from carrying out a process on our behalf without authorization,
+            and creating false identities or commit fraud.
+          </Typography>
+          <div>
+            <Typography className="top-question">How it works</Typography>
+            <div className="pb-8">
+              <Row gutter={40}>
+                <Col xxl={8} xl={8} lg={8} md={8} xs={24}>
+                  <center>
+                    <img src={step1} alt="" />
+                    <Typography className="stepnumber pt-2 pb-2">Step 1</Typography>
+                    <Typography className="stepdescription">Take a photo of your identity document</Typography>
+                  </center>
+                </Col>
+                <Col xxl={8} xl={8} lg={8} md={8} xs={24}>
+                  <center>
+                    <img src={step2} alt="" />
+                    <Typography className="stepnumber pt-2 pb-2">Step 2</Typography>
+                    <Typography className="stepdescription">Take a self-portrait  photo
+                      using your phone’s camera or
+                      desktop webcam</Typography>
+                  </center>
+                </Col>
+                <Col xxl={8} xl={8} lg={8} md={8} xs={24}>
+                  <center className="pt-5">
+                    <img src={step3} alt="" />
+                    <Typography className="stepnumber pt-7 pb-2">Step 3</Typography>
+                    <Typography className="stepdescription">Your photos and ID are verified
+                      with our system</Typography>
+                  </center>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
