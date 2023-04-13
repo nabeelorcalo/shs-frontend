72f5f922f1d0760e3c14ecf66b154798e327ab49
Muhammad Noman <muhammad.noman@ceative.co.uk>
2023-04-13 12:06:10 +0500

    Worked on Drawer Style

--- src/pages/leaves/intern/calendar/calendarDataDrawer.tsx
+++ src/pages/leaves/intern/calendar/calendarDataDrawer.tsx
@@ -7,6 +7,18 @@ const CalendarDataDrawer = (props: any) => {
     const eventRange = eventData?.event?._instance?.range
     const extendedPropsData = eventData?.event?._def?.extendedProps
     console.log('extendedPropsData', extendedPropsData);
+    const renderBgColor:any = {
+        "Sick": "rgba(76, 164, 253, 0.25)",
+        "Casual": "rgba(255, 193, 93, 0.25)",
+        "Work from home": "rgba(233, 111, 124, 0.25)",
+        "Medical": "rgba(106, 173, 142, 0.25)"
+    }
+    const spanBGColorRender:any ={
+        "Sick": "rgba(76, 164, 253, 1)",
+        "Casual": "rgba(255, 193, 93, 1)",
+        "Work from home": "rgba(233, 111, 124, 1)",
+        "Medical": "rgba(106, 173, 142, 1)"
+    }
     // console.log('events', events);
     return (
         <DrawerComp
@@ -24,12 +36,8 @@ const CalendarDataDrawer = (props: any) => {
                 requestedOn={eventRange?.start}
                 aprover={extendedPropsData?.aprover}
                 ApprovedBy={extendedPropsData?.ApprovedBy}
-                backgroundColor={events?.title === "Sick" ?
-                    "rgba(76, 164, 253, 0.25)" : events?.title === "Casual" ?
-                        "rgba(255, 193, 93, 0.25)" : events?.title === "Work from home" ? "rgba(233, 111, 124, 0.25)" : "rgba(106, 173, 142, 0.25)"}
-                spanBG={events?.title === "Sick" ?
-                    "rgba(76, 164, 253, 1)" : events?.title === "Casual" ?
-                        "rgba(255, 193, 93, 1)" : events?.title === "Work from home" ? "rgba(233, 111, 124, 1)" : "rgba(106, 173, 142, 1)"}
+                backgroundColor={renderBgColor[events?.title]}
+                spanBG={spanBGColorRender[events?.title]} 
                 title={events?.title}
                 dateFrom={eventRange?.start}
                 dateTo={eventRange?.end}
