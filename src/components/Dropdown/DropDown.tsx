664130c794e8b22db808a40cc254ef6388c7275b
arsalan <arsalan.tahir@ceative.co.uk>
2023-04-17 11:05:43 +0500

    space indenration done

--- src/components/Dropdown/DropDown.tsx
+++ src/components/Dropdown/DropDown.tsx
@@ -9,110 +9,110 @@ import './style.scss';
 import CommonRangePicker from '../calendars/CommonDatePicker/CommonRangePicker';
 
 export const DropDown = (props: DropDownInterface) => {
-    const {
-        name = 'this month',
-        value,
-        options = [],
-        requireSearchBar = false,
-        requireCheckbox = false,
-        requireDatePicker = false,
-        checkboxOnRight = false,
-        searchValue = '',
-        placement = 'bottomRight',
-        setValue,
-        pilled = false,
-        showDatePickerOnVal,
-        setSearchValue,
-        selectedList = [],
-        setSelectedList,
-        startIcon,
-        endIcon: EndIcon = ArrowDownDark,
-        requiredDownloadIcon,
-        requireRangePicker,
-    } = props;
+  const {
+    name = 'this month',
+    value,
+    options = [],
+    requireSearchBar = false,
+    requireCheckbox = false,
+    requireDatePicker = false,
+    checkboxOnRight = false,
+    searchValue = '',
+    placement = 'bottomRight',
+    setValue,
+    pilled = false,
+    showDatePickerOnVal,
+    setSearchValue,
+    selectedList = [],
+    setSelectedList,
+    startIcon,
+    endIcon: EndIcon = ArrowDownDark,
+    requiredDownloadIcon,
+    requireRangePicker,
+  } = props;
 
-    const [visible, setVisible] = useState(false);
-    const [openPicker, setOpenPicker] = useState(false);
-    const [openRangePicker, setOpenRangePicker] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [openPicker, setOpenPicker] = useState(false);
+  const [openRangePicker, setOpenRangePicker] = useState(false);
 
-    const items = options?.map((option: string, i: number) => {
-        if (requireSearchBar && option === 'search') {
-            return {
-                label: <SearchBar
-                    size='middle'
-                    value={searchValue}
-                    name='searchbar'
-                    handleChange={setSearchValue}
-                />,
-                key: option
-            }
-        }
-        if (requireCheckbox) {
-            return {
-                label: <div className={`flex  gap-5 ${checkboxOnRight ? 'justify-between flex-row-reverse' : ''}`}>
-                    <Checkbox id={option} name={option}
-                        onChange={(e) => handleCheckbox(e, option, setSelectedList)}
-                        checked={selectedList.includes(option)}
-                    >
-                    </Checkbox>
-                    <label htmlFor={option} className='capitalize cursor-pointer'>{option}</label>
-                </div>,
-                key: option
-            }
-        }
-        if (requireDatePicker && option === showDatePickerOnVal) {
-            return {
-                label:
-                    <CommonDatePicker
-                        requireAsButton
-                        btnClassName='drop-down-btn'
-                        name={option}
-                        open={openPicker}
-                        setOpen={setOpenPicker}
-                        dropdownClassName='picker-extra-class'
-                        placement='bottomLeft'
-                        setValue={setValue}
-                    />,
-                key: option
-            }
-        }
-        if (requireRangePicker && option === showDatePickerOnVal) {
-            return {
-                label: <CommonRangePicker
-                    className='common-range-picker-wrapper'
-                    open={openRangePicker}
-                    setOpen={setOpenRangePicker}
-                    option={option}
-                    onChange={(_: any, val: string[]) => setValue(val.toString().split(',').join(' , '))}
-                />,
-                key: option
-            }
-        }
-        return {
-            label: <span className='capitalize' onClick={() => setValue(option)}>{option}</span>,
-            key: option
-        }
-    });
+  const items = options?.map((option: string, i: number) => {
+    if (requireSearchBar && option === 'search') {
+      return {
+        label: <SearchBar
+          size='middle'
+          value={searchValue}
+          name='searchbar'
+          handleChange={setSearchValue}
+        />,
+        key: option
+      }
+    }
+    if (requireCheckbox) {
+      return {
+        label: <div className={`flex  gap-5 ${checkboxOnRight ? 'justify-between flex-row-reverse' : ''}`}>
+          <Checkbox id={option} name={option}
+            onChange={(e) => handleCheckbox(e, option, setSelectedList)}
+            checked={selectedList.includes(option)}
+          >
+          </Checkbox>
+          <label htmlFor={option} className='capitalize cursor-pointer'>{option}</label>
+        </div>,
+        key: option
+      }
+    }
+    if (requireDatePicker && option === showDatePickerOnVal) {
+      return {
+        label:
+          <CommonDatePicker
+            requireAsButton
+            btnClassName='drop-down-btn'
+            name={option}
+            open={openPicker}
+            setOpen={setOpenPicker}
+            dropdownClassName='picker-extra-class'
+            placement='bottomLeft'
+            setValue={setValue}
+          />,
+        key: option
+      }
+    }
+    if (requireRangePicker && option === showDatePickerOnVal) {
+      return {
+        label: <CommonRangePicker
+          className='common-range-picker-wrapper'
+          open={openRangePicker}
+          setOpen={setOpenRangePicker}
+          option={option}
+          onChange={(_: any, val: string[]) => setValue(val.toString().split(',').join(' , '))}
+        />,
+        key: option
+      }
+    }
+    return {
+      label: <span className='capitalize' onClick={() => setValue(option)}>{option}</span>,
+      key: option
+    }
+  });
 
-    return (
-        <AntDropDown
-            menu={{ items }}
-            trigger={['click']}
-            open={visible}
-            placement={placement}
-            className={`drop-down-wrapper ${visible && 'active'} ${pilled && 'pilled'}`}
-            overlayClassName='drop-down-overlay'
-            onOpenChange={setVisible}
-            {...props}
-        >
-            <div className='flex items-center justify-between'>
-                <p className='flex items-center'>
-                    {startIcon && <img src={startIcon} alt='icon' style={{ marginRight: '20px' }} />}
-                    {!requiredDownloadIcon && <span className='mr-[20px]'>{value ? value : name}</span>}
-                </p>
-                {!requiredDownloadIcon && <EndIcon className='ml-[10px]' />}
-                {requiredDownloadIcon && <div className='mr-[-5px]'><DownloadIconLeave /></div>}
-            </div>
-        </AntDropDown>
-    )
+  return (
+    <AntDropDown
+      menu={{ items }}
+      trigger={['click']}
+      open={visible}
+      placement={placement}
+      className={`drop-down-wrapper ${visible && 'active'} ${pilled && 'pilled'}`}
+      overlayClassName='drop-down-overlay'
+      onOpenChange={setVisible}
+      {...props}
+    >
+      <div className='flex items-center justify-between'>
+        <p className='flex items-center'>
+          {startIcon && <img src={startIcon} alt='icon' style={{ marginRight: '20px' }} />}
+          {!requiredDownloadIcon && <span className='mr-[20px]'>{value ? value : name}</span>}
+        </p>
+        {!requiredDownloadIcon && <EndIcon className='ml-[10px]' />}
+        {requiredDownloadIcon && <div className='mr-[-5px]'><DownloadIconLeave /></div>}
+      </div>
+    </AntDropDown>
+  )
 }
--- src/components/calendars/CommonDatePicker/CommonRangePicker.tsx
+++ src/components/calendars/CommonDatePicker/CommonRangePicker.tsx
@@ -4,19 +4,19 @@ const { RangePicker } = DatePicker;
 import './common-range-picker.scss';
 
 const CommonRangePicker = (props: any) => {
-    const { option, className, open, setOpen, onChange, popupClassName } = props;
-    return (
-        <div className='relative'>
-            <span className='capitalize' onClick={() => setOpen(!open)}>{option}</span>
-            <RangePicker
-                className={className}
-                open={open}
-                onOpenChange={() => setOpen(!open)}
-                onChange={onChange}
-                popupClassName={`${popupClassName} custom-range-picker-popup`}
-            />
-        </div>
-    )
+  const { option, className, open, setOpen, onChange, popupClassName } = props;
+  return (
+    <div className='relative'>
+      <span className='capitalize' onClick={() => setOpen(!open)}>{option}</span>
+      <RangePicker
+        className={className}
+        open={open}
+        onOpenChange={() => setOpen(!open)}
+        onChange={onChange}
+        popupClassName={`${popupClassName} custom-range-picker-popup`}
+      />
+    </div>
+  )
 }
 
 export default CommonRangePicker
