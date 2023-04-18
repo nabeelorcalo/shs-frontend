728c876415b795e916cadd5a8a174bdadad54089
Shayan ul haq <Shayan.ulhaq@ceative.co.uk>
2023-04-18 06:59:00 +0000

    Squashed commit of the following
    commit 44d70e32 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Tue Apr 18 2023 11:14:23 GMT+0500 (Pakistan Standard Time) 
    
        #merged with dev
    
    
    commit 35de8e99 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Tue Apr 18 2023 10:53:52 GMT+0500 (Pakistan Standard Time) 
    
        #comment merging with dev
    
    
    commit 57476c2f 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Mon Apr 17 2023 11:57:35 GMT+0500 (Pakistan Standard Time) 
    
        #comment merge with dev
    
    
    commit df9a0252 
    Author: Shayan ul haq <Shayan.ulhaq@ceative.co.uk> 
    Date: Fri Apr 14 2023 12:02:46 GMT+0500 (Pakistan Standard Time) 
    
        comment new chnging

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -600,7 +600,8 @@ export { ReactComponent as SearchIconJob } from './serachJobs/search-normal.svg'
 export { ReactComponent as LocationIconJob } from './serachJobs/location.svg';
 export { ReactComponent as SearchFilter } from './serachJobs/Filter.svg';
 export { ReactComponent as SeachJobArrow } from './serachJobs/Vector.svg';
-export { ReactComponent as TabIcon1 } from './serachJobs/tab1.svg';
+export { ReactComponent as TabIcon } from './serachJobs/tab1.svg';
+// export { default as TabIcon1 } from '../../assets/images/documents/tabcase.svg';
 export { ReactComponent as TabIcon2 } from './serachJobs/tab2.svg';
 export { ReactComponent as TabIcon3 } from './serachJobs/tab3.svg';
 export { ReactComponent as TabIcon4 } from './serachJobs/tab4.svg';
@@ -625,6 +626,7 @@ export { default as Page404 } from '../../assets/images/404/404.svg';
 //Intern Douments 
 export { default as Folder } from '../../assets/images/documents/folder.svg';
 export { default as UploadIconBtn } from '../../assets/images/documents/document-upload.svg';
-export { default as Star } from '../../assets/images/documents/star.svg';
 export { default as Dots } from '../../assets/images/documents/dots.svg';
 export { default as DoucmentCard } from '../../assets/images/documents/Document Card.svg';
+export { default as DocImage } from '../../assets/images/documents/Document Card (1).svg';
+export { default as Frame } from '../../assets/images/documents/frame.svg';
