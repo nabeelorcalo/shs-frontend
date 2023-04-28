12f928344e2fe3c18c906769018fb7ccb6054cf5
ayaz.khan <ayaz.khan@ceative.co.uk>
2023-04-28 15:09:48 +0500

    close icon and docs icons added to upload cards

--- src/components/SelectedUploadCard/index.tsx
+++ src/components/SelectedUploadCard/index.tsx
@@ -1,23 +1,32 @@
 import { CloseCircleFilled } from '@ant-design/icons'
 import React from 'react'
-import { DocumentCardIcon } from '../../assets/images'
+import { DocumentCardIcon, DocImage } from '../../assets/images'
 
 const SelectedUploadCard = ({ filename, filesize, idx, handleDelete }: any) => {
+
+    const extension = filename.slice(-3)
+    console.log(extension)
     return (
         <div className="flex w-6/12 ">
             <div className=' flex flex-row justify-between items-center m-1 w-full px-2 py-2  rounded bg-[#E6F4F9]'>
                 <div className='flex flex-row gap-2 items-center'>
                     <div>
-                        <DocumentCardIcon style={{fontSize:'42px'}} />
+                        {
+                            extension === "pdf" ?
+                                <DocumentCardIcon style={{ fontSize: '42px' }} /> :
+                                extension === "ocx" ?
+                                    <img src={DocImage} alt="" width='46px' />
+                                    : null
+                        }
                     </div>
                     <div className='flex flex-col justify-evenly gap-2'>
                         <div className='text-md'>{filename}</div>
                         <div className="text-sm">{filesize} MB</div>
                     </div>
                 </div>
-                {/* <div>
-                    <CloseCircleFilled onClick={()=>{handleDelete(idx)}} />
-                </div> */}
+                <div>
+                    <CloseCircleFilled onClick={() => { handleDelete(idx) }} />
+                </div>
             </div>
         </div>
 
