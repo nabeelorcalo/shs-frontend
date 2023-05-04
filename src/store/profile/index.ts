718c0837375b9108cc2a1b958199c2a1bd5c8ddb
developer_shahzeb.rehman <shahzebrehman.khattak@ceative.co.uk>
2023-05-04 14:02:34 +0500

    inprofilestudent

--- src/store/profile/index.ts
+++ src/store/profile/index.ts
@@ -3,8 +3,16 @@ import {
   atom,
   selector,
 } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const someState = atom({
   key: 'someState', // unique ID (with respect to other atoms/selectors)
   default: '', // default value
+});
+export const changepasswordstate = atom({
+  key: "changepasswordstate",
+  default: {},   // {} || [] 
+  effects_UNSTABLE: [persistAtom],
 });
