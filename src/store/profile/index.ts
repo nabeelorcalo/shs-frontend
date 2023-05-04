085e0d886232922b394130cf30659ede9f43e42d
developer_shahzeb.rehman <shahzebrehman.khattak@ceative.co.uk>
2023-05-04 14:50:03 +0500

    camelcase

--- src/store/profile/index.ts
+++ src/store/profile/index.ts
@@ -1,11 +1,7 @@
 import React from "react";
 import { atom, selector } from "recoil";
 
-export const someState = atom({
-  key: "someState", // unique ID (with respect to other atoms/selectors)
-  default: "", // default value
-});
-export const changepasswordstate = atom({
-  key: "changepasswordstate",
+export const changePasswordState = atom({
+  key: "changePasswordState",
   default: {}, // {} || []
 });
