47aa904f7224ee414f9aa4db7001d7380d40aeea
Muhammad Shoaib <muhammad.shoaib@ceative.co.uk>
2023-05-05 16:02:59 +0500

    fixes in candidates module

--- src/components/HiringPIpeline/hiringPipeline.tsx
+++ src/components/HiringPIpeline/hiringPipeline.tsx
@@ -1,63 +1,65 @@
-import "./Pipeline.scss"
+import "./Pipeline.scss";
 
 const Array = [
-    {
-        title: "applied",
-        value: "0",
-        color: "#363565"
-    }, {
-        title: "interviewed",
-        value: "0",
-        color: "#D2D6DC"
-
-
-    }, {
-        title: "recommended",
-        value: "0",
-        color: "#D2D6DC"
-
-
-    }, {
-        title: "offer letter",
-        value: "0",
-        color: "#D2D6DC"
-
-    }, {
-        title: "contract",
-        value: "0",
-        color: "#D2D6DC"
-
-    }, {
-        title: "hired",
-        value: "0",
-        color: "#D2D6DC"
-
-    }, {
-        title: "rejected",
-        value: "0",
-        color: "#D2D6DC"
-
-    }
-]
+  {
+    title: "applied",
+    value: "0",
+    color: "#363565",
+  },
+  {
+    title: "interviewed",
+    value: "0",
+    color: "#D2D6DC",
+  },
+  {
+    title: "recommended",
+    value: "0",
+    color: "#D2D6DC",
+  },
+  {
+    title: "offer letter",
+    value: "0",
+    color: "#D2D6DC",
+  },
+  {
+    title: "contract",
+    value: "0",
+    color: "#D2D6DC",
+  },
+  {
+    title: "hired",
+    value: "0",
+    color: "#D2D6DC",
+  },
+  {
+    title: "rejected",
+    value: "0",
+    color: "#D2D6DC",
+  },
+];
 const HiringPipeline = (props: any) => {
-    const { hiringList } = props;
+  const { hiringList, handleHiringProcess } = props;
+  const handleClick = (pipeline: string) => {
+    handleHiringProcess(pipeline);
+  };
+  return (
+    <div className="pipeline-wrapper">
+      {Array.filter((item) => (hiringList?.includes("hired") && item?.title === "rejected" ? null : item)).map(
+        (item: any) => (
+          <div
+            style={{
+              backgroundColor: hiringList.includes(item?.title) ? "#363565" : "#D2D6DC",
+              color: hiringList.includes(item?.title) ? "#fff" : "",
+            }}
+            className={`pipline-content capitalize flex justify-center items-center`}
+            onClick={() => handleClick(item.title)}
+          >
+            {item.title}
+          </div>
+        )
+      )}
+    </div>
+  );
+};
 
-    return (
-
-        <div className='pipeline-wrapper'>
-            {Array.map((item: any) => (
-                <div style={{
-                    backgroundColor: hiringList.includes(item?.title) ? '#363565' : '#D2D6DC',
-                    color: hiringList.includes(item?.title) ? '#fff' : ''
-                }}
-                    className={`pipline-content capitalize flex justify-center items-center`}>
-                    {item.title}
-                </div>
-            ))
-            }
-        </div >
-    )
-}
-
-export default HiringPipeline
-
+export default HiringPipeline;
