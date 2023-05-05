c6621be233c9885698c0df81459407b41fcaf3cd
ayaz.khan <ayaz.khan@ceative.co.uk>
2023-05-05 08:55:44 +0500

    indentation fixed for selected upload card file

--- src/components/SelectedUploadCard/index.tsx
+++ src/components/SelectedUploadCard/index.tsx
@@ -3,35 +3,35 @@ import React from 'react'
 import { DocumentCardIcon, DocImage } from '../../assets/images'
 
 const SelectedUploadCard = (props: any) => {
-    const { files, setFiles, filename, filesize, idx, } = props
-    const extension = filename.slice(-3)
-    const handleDelete = (idx: any) => {
-        setFiles(files.splice(idx, 1));
-    }
-    return (
-        <div className="flex w-6/12 ">
-            <div className=' flex flex-row justify-between items-center m-1 w-full px-2 py-2  rounded bg-[#E6F4F9]'>
-                <div className='flex flex-row gap-2 items-center'>
-                    <div>
-                        {
-                            extension === "pdf" ?
-                                <DocumentCardIcon style={{ fontSize: '42px' }} /> :
-                                extension === "ocx" ?
-                                    <img src={DocImage} alt="" width='46px' />
-                                    : null
-                        }
-                    </div>
-                    <div className='flex flex-col justify-evenly gap-2'>
-                        <div className='text-md'>{filename}</div>
-                        <div className="text-sm">{filesize} MB</div>
-                    </div>
-                </div>
-                <div>
-                    <CloseCircleFilled onClick={() => { handleDelete(idx) }} />
-                </div>
-            </div>
+  const { files, setFiles, filename, filesize, idx, } = props
+  const extension = filename.slice(-3)
+  const handleDelete = (idx: any) => {
+    setFiles(files.splice(idx, 1));
+  }
+  return (
+    <div className="flex w-6/12 ">
+      <div className=' flex flex-row justify-between items-center m-1 w-full px-2 py-2  rounded bg-[#E6F4F9]'>
+        <div className='flex flex-row gap-2 items-center'>
+          <div>
+            {
+              extension === "pdf" ?
+                <DocumentCardIcon style={{ fontSize: '42px' }} /> :
+                extension === "ocx" ?
+                  <img src={DocImage} alt="" width='46px' />
+                  : null
+            }
+          </div>
+          <div className='flex flex-col justify-evenly gap-2'>
+            <div className='text-md'>{filename}</div>
+            <div className="text-sm">{filesize} MB</div>
+          </div>
         </div>
-    )
+        <div>
+          <CloseCircleFilled onClick={() => { handleDelete(idx) }} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SelectedUploadCard
