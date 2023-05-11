a57e0a6a7107458186b08a7e155a61c4c040daa6
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-05-11 08:59:29 +0000

    Squashed commit of the following
    commit 0533a063 
    Author: Azeem Aslam <azeem.aslam@ceative.co.uk> 
    Date: Thu May 11 2023 13:48:31 GMT+0500 (Pakistan Standard Time) 
    
        implement  contract and offer letter API in student mod
    
    
    commit fe2f109f 
    Author: Azeem Aslam <azeem.aslam@ceative.co.uk> 
    Date: Thu May 11 2023 08:16:08 GMT+0500 (Pakistan Standard Time) 
    
        time frame filter implement in offer letter
    
    
    commit d7600906 
    Author: Azeem Aslam <azeem.aslam@ceative.co.uk> 
    Date: Wed May 10 2023 20:27:38 GMT+0500 (Pakistan Standard Time) 
    
        #comment student module conntract list show

--- src/pages/contracts/actionHandler.ts
+++ src/pages/contracts/actionHandler.ts
@@ -1,27 +1,34 @@
+import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { contractsListData } from "../../store";
 import endpoints from "../../config/apiEndpoints";
 import { Notifications } from "../../components";
 import api from "../../api";
+import dayjs from "dayjs";
 
 // Chat operation and save into store
 const useCustomHook = () => {
   const { GET_CONTRACT_LIST, DEL_CONTRACT } = endpoints;
   const [contractList, setContractList] = useRecoilState(contractsListData);
+  const [loading, setLoading] = useState(false);
+  const todayDate = dayjs(new Date()).format("YYYY-MM-DD");
 
   //get contracts
-  const getContractList = async (status: any,time:any) => {
+  const getContractList = async (status: any, time: any) => {
     const params = {
       page: 1,
       limit: 10,
-      status: status? status: null,
+      status: status ? status : null,
       type: 'CONTRACT',
-      currentDate: '2023-05-09',
-      filterType: time ? time : null
+      currentDate: todayDate,
+      filterType: time ? time : null,
     }
-    let query = Object.entries(params).reduce((a:any, [k, v]) => (v ? ((a[k] = v), a) : a), {});
+    let query = Object.entries(params).reduce((a: any, [k, v]) => (v ? ((a[k] = v), a) : a), {});
     const { data } = await api.get(GET_CONTRACT_LIST, query);
     setContractList(data)
+    if (data) {
+      setLoading(true)
+    }
   };
 
   //search contracts
@@ -37,17 +44,23 @@ const useCustomHook = () => {
     }
     const { data } = await api.get(GET_CONTRACT_LIST, params);
     setContractList(data);
+    if (data) {
+      setLoading(true)
+    }
   }
 
   //delete contracts
   const deleteContractHandler = async (val: any) => {
-    const { data } = await api.delete(DEL_CONTRACT, `${DEL_CONTRACT}/${val}`);
-    // getContractList;
-    console.log("delete data are ", data);
-    Notifications({ title: 'Success', description: 'Contract deleted', type: 'success' })
+    const { data } = await api.delete(`${DEL_CONTRACT}/${val}`);
+    if (data) {
+      getContractList(null, 'THIS_MONTH')
+      let query = Object.entries(val).reduce((a: any, [k, v]) => (v ? ((a[k] = v), a) : a), {});
+      Notifications({ title: 'Success', description: 'Contract deleted', type: 'success' })
+      setLoading(true)
+    }
   }
   return {
-    getContractList, contractList, searchHandler, deleteContractHandler
+    getContractList, contractList, searchHandler, deleteContractHandler,loading
   };
 };
 
