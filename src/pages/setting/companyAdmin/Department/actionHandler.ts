d4d904b7e9ac686dbfeb578ad854dd34a9492535
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-05-22 22:32:30 +0500

    #comment edit department done

--- src/pages/setting/companyAdmin/Department/actionHandler.ts
+++ src/pages/setting/companyAdmin/Department/actionHandler.ts
@@ -3,34 +3,39 @@ import { useRecoilState } from "recoil";
 import api from "../../../../api";
 import apiEndpints from "../../../../config/apiEndpoints";
 import { settingDepartmentState } from "../../../../store";
+import { Notifications } from "../../../../components";
 
 const useCustomHook = () => {
   const { SETTING_DAPARTMENT } = apiEndpints;
-  const [settingDepartmentdata, setSettingDepartmentdata] = useRecoilState(settingDepartmentState); 
+  const [settingDepartmentdata, setSettingDepartmentdata] = useRecoilState(settingDepartmentState);
 
   // get setting departments
-  const getSettingDepartment = async (page: any, q: any): Promise<any> => {
-    const param = { page: page, limit: 10, q: q }
+  const getSettingDepartment = async (q: any): Promise<any> => {
+    const param = { page: 1, limit: 10, q: q }
     const { data } = await api.get(SETTING_DAPARTMENT, param);
     setSettingDepartmentdata(data)
   };
 
   const deleteSettingDepartment = async (id: number) => {
     await api.delete(`${SETTING_DAPARTMENT}/${id}`);
-    getSettingDepartment(null, null)
+    getSettingDepartment(null)
   };
 
   // post setting departments
   const postSettingDepartment = async (body: any) => {
     await api.post(SETTING_DAPARTMENT, body);
-    getSettingDepartment(null, null)
+    getSettingDepartment(null)
   };
 
   // edit setting departments
-  const patchSettingDepartment = async (body: any) => {
-    console.log(body);
-    await api.patch(SETTING_DAPARTMENT, body);
-    getSettingDepartment(null, null)
+  const patchSettingDepartment = async (values: any, id: any) => {
+    const params = {
+      name: values.departmentName,
+      description: values.description
+    }
+    await api.patch(`${SETTING_DAPARTMENT}/${id}`, params);
+    getSettingDepartment(null)
+    Notifications({ title: 'Success', description: 'Department edited successfully', 'type': 'success' })
   };
 
   return {
--- src/pages/setting/companyAdmin/Department/index.tsx
+++ src/pages/setting/companyAdmin/Department/index.tsx
@@ -1,99 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { DepartmentAddIcon } from "../../../../assets/images";
-import { Col, Row, Typography, Button, Form } from "antd";
-import { Input } from "antd";
+import { Col, Row, Typography, Button } from "antd";
 import { Alert, SearchBar } from "../../../../components";
 import DropDownForSetting from "../../../../components/Setting/Common/CustomSettingDropdown";
-import { PopUpModal } from "../../../../components/Model";
 import useCustomHook from "./actionHandler";
 import { useRecoilState } from "recoil";
 import { settingDepartmentState } from "../../../../store";
+import AddNewDepaertmentModal from "./addNewDepaertmentModal";
 import "./style.scss";
 
-const { TextArea } = Input;
-const { Text } = Typography;
-const demoData = [
-  {
-    name: "Finance",
-    description: "The finance department is responsible for the financial planning and management of the company."
-  },
-  {
-    name: "Human Resources",
-    description: "The human resources department is responsible for a company's most important asset: its people."
-  },
-  {
-    name: "Design",
-    description: "Designers are responsible for the design and implementation of all the experiences."
-  },
-]
 const SettingDepartment: React.FC = () => {
-  const action = useCustomHook();
+  const { getSettingDepartment, deleteSettingDepartment } = useCustomHook();
   const departmentData = useRecoilState<any>(settingDepartmentState)
-  const [form] = Form.useForm();
-  const [edit, setEdit] = useState<any>()
+  const [edit, setEdit] = useState<any>({})
+  // const [deleteModal, setDeleteModal] = useState<any>(false)
   const [state, setState] = useState<any>(
     {
-      showEditModal: false,
-      showDeleteModal: false,
+      isEditModal: false,
+      isDeleteModal: false,
       departmentId: "",
     }
   )
 
-  const handleChange = (event: any) => {
-    action.getSettingDepartment(1, event)
-  };
-
-  if (edit?.isEdit === "isEdit") {
-    const formValues = { departmentName: edit?.name, description: edit?.description }
-    form.setFieldsValue(formValues)
-  }
-
-  const onFinish = (values: any) => {
-    console.log(values);
-
-    if (edit?.isEdit === "isEdit") {
-
-    }
-    else {
-      const { departmentName, description } = values
-      action.postSettingDepartment({
-        name: departmentName,
-        description: description
-      }).then(() => { action.getSettingDepartment(1, "") })
-      form.resetFields()
-      setState({ ...state, showEditModal: false })
-    }
-  };
-  console.log("Edit", edit)
-
-  const SetId = (id: any) => {
-    setState({ ...state, showDeleteModal: !state.showDeleteModal, departmentId: id })
-  }
-
-  const SetEditData = (editdata: any) => {
-    setEdit(editdata)
-  }
-  const DeleleHandler = () => {
-    action.deleteSettingDepartment(state.departmentId)
-  }
-
-  const resetFormHandler = () => {
-    setState({ ...state, showEditModal: false })
-    form.resetFields()
-    setEdit("")
-  }
-
   useEffect(() => {
-    action.getSettingDepartment(1, "")
+    getSettingDepartment(null)
   }, [])
 
   return (
     <div className="setting-department">
       <div className="flex justify-between location-header">
-        <SearchBar size="middle" handleChange={handleChange} />
+
+        <SearchBar size="middle" handleChange={() => getSettingDepartment(event)} />
         <Button
           size="middle"
-          onClick={() => { setState({ ...state, showEditModal: !state.showEditModal }); }}
+          onClick={() => { setState({ ...state, isEditModal: true }); }}
           className="flex gap-2 setting-add-button white-color teriary-bg-color"
         >
           <DepartmentAddIcon /> Add Department
@@ -106,32 +46,29 @@ const SettingDepartment: React.FC = () => {
               <div className="department-box-wrapper">
                 <div className="flex justify-between">
                   <div>
-                    <Text className="text-lg font-semibold text-primary-color">
+                    <p className="text-lg font-semibold text-primary-color">
                       {data?.name}
-                    </Text>
+                    </p>
                   </div>
                   <div className="float-right cursor-pointer">
                     <DropDownForSetting
-                      showEditModal={state.showEditModal}
-                      showDeleteModal={state.showDeleteModal}
-                      setShowDeleteModal={setState}
-                      setShowEditModal={setState}
+                      state={state}
+                      setState={setState}
                       id={data?.id}
-                      SetId={SetId}
                       editData={data}
-                      SetEditData={SetEditData}
+                      SetEditData={setEdit}
                     />
                   </div>
                 </div>
-                <Text className="text-sm font-normal text-secondary-color ">
+                <p className="text-sm font-normal text-secondary-color ">
                   {data?.description}
-                </Text>
+                </p>
               </div>
             </Col>
           );
         })
         }
-        {
+        {/* {
           departmentData[0] && !Array.isArray(departmentData[0]) && <Col className="gutter-row" xs={24} xl={12} xxl={8}>
             <div className="department-box-wrapper">
               <div className="flex justify-between">
@@ -147,8 +84,8 @@ const SettingDepartment: React.FC = () => {
                     setShowDeleteModal={setState}
                     setShowEditModal={setState}
                     id={departmentData[0]?.id}
-                    SetId={SetId}
-                    SetEditData={SetEditData}
+                    SetId={setState}
+                    SetEditData={setEdit}
                     state={state}
                   />
                 </div>
@@ -158,58 +95,16 @@ const SettingDepartment: React.FC = () => {
               </Text>
             </div>
           </Col>
-        }
+        } */}
       </Row>
-      <PopUpModal
-        open={state.showEditModal}
-        title="Department"
-        width={600}
-        close={() => setState({ ...state, showEditModal: false })}
-        footer=""
-      >
-        <Form
-          layout="vertical"
-          form={form}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            label="Department Name"
-            name="departmentName"
-          >
-            <Input
-              className="input"
-              id="departmentName"
-              placeholder="Enter department name "
-              size="middle"
-              type="text"
-            />
-          </Form.Item>
-          <Form.Item
-            label="Description"
-            name="description"
-          >
-            <TextArea rows={6} placeholder="Write Something..."
-              id="description"
-              name="description"
-              size="middle"
-            />
-          </Form.Item>
-          <div className="setting-department-footer flex justify-end mt-4 gap-2">
-            <Button key="Cancel" className="footer-cancel-btn "
-              onClick={resetFormHandler}>
-              Cancel
-            </Button>
-            <Button key="submit" className="footer-submit-btn" htmlType="submit">
-              Submit
-            </Button>
-          </div>
-        </Form>
-      </PopUpModal>
+      {state.isEditModal &&
+        <AddNewDepaertmentModal state={state} setState={setState} edit={edit} setEdit={setEdit} />
+      }
       <Alert
         cancelBtntxt="Cancel"
         okBtntxt="Delete"
-        okBtnFunc={DeleleHandler}
-        state={state.showDeleteModal}
+        okBtnFunc={() => deleteSettingDepartment(state.departmentId)}
+        state={state.isDeleteModal}
         setState={setState}
         type="error"
         width={500}
