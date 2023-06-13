2113613bc97afa13fa1f8b0136310b2236f60e67
Muhammad Shoaib <muhammad.shoaib@ceative.co.uk>
2023-06-13 12:50:59 +0500

    fixes in dashboard

--- src/pages/dashboard/companyAdmin/index.tsx
+++ src/pages/dashboard/companyAdmin/index.tsx
@@ -62,7 +62,6 @@ const CompanyAdmin = () => {
     setIsShowModal(true);
   };
   const handleSelect = (value: string) => {
-    console.log(value);
     getInternShipList(value === "all" ? "" : value);
   };
   useEffect(() => {
@@ -100,7 +99,12 @@ const CompanyAdmin = () => {
       />
       <Row gutter={gutter}>
         <Col xs={24} xl={15} xxl={17}>
-          <PiplineTable internshipsList={internshipsList} handleSelect={handleSelect} departmentList={departmentList} loading={isLoading} />
+          <PiplineTable
+            internshipsList={internshipsList}
+            handleSelect={handleSelect}
+            departmentList={departmentList}
+            loading={isLoading}
+          />
         </Col>
         <Col xs={24} xl={9} xxl={7}>
           <InternshipSummaryChart
@@ -185,7 +189,7 @@ const CompanyAdmin = () => {
         <Col xs={24} sm={24} xl={24} xxl={5}>
           <Row gutter={gutter}>
             <Col xs={24} xl={12} xxl={24}>
-              <TopPerformers topPerformersList={topPerformerList} user={Constants?.COMPANY_ADMIN} />
+              <TopPerformers topPerformersList={topPerformerList} user={Constants?.COMPANY_ADMIN} loading={isLoading} />
             </Col>
             <Col xs={24} xl={12} xxl={24}>
               <LeaveDetails
@@ -218,7 +222,7 @@ const CompanyAdmin = () => {
           </Row>
         </Col>
       </Row>
-      <AnnouncementModal isShowModal={isShowModal} close={() => setIsShowModal(false)} />
+      {isShowModal && <AnnouncementModal isShowModal={isShowModal} close={() => setIsShowModal(false)} />}
     </>
   );
 };
