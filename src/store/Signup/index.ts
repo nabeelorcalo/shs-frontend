b94ad2837f4fac9abc690d7bdba765bff9c20b15
Deft <khubaib.op@gmail.com>
2023-06-14 23:47:11 +0500

    atom completed

--- src/store/Signup/index.ts
+++ src/store/Signup/index.ts
@@ -3,5 +3,21 @@ import { recoilPersist } from "recoil-persist";
 
 export const companyStepperData = atom({
   key: "companyStepperData",
-  default: {},
+  default: {
+    businessType: "",
+    businessSector: "",
+    businessName: "",
+    registrationNumber: "",
+    countryOfIncorporation: "",
+    dateOfIncorporation: "",
+    postCode: "",
+    address: "",
+    street: "",
+    town: "",
+    country: "",
+    ownerName: "",
+    ownerRole: "",
+    ownerAddress: "",
+    ownerDOB: "",
+  },
 });
