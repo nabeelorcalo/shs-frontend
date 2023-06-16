483cdc38cc35a3cb988dfed1b242f546d62851ed
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-06-16 02:58:17 -0700

    #comment global user selector search implemented

--- src/components/UserSelector/index.tsx
+++ src/components/UserSelector/index.tsx
@@ -1,6 +1,8 @@
 import { Avatar, Input, Select, Space } from 'antd';
 import { GlassMagnifier } from '../../assets/images';
 import './styles.scss'
+import { SearchBar } from '../SearchBar/SearchBar';
+import { useState } from 'react';
 
 const { Option } = Select;
 
@@ -23,10 +25,16 @@ interface UserSelectorProps {
 const UserSelector = (props: UserSelectorProps) => {
   const { label, value, onChange, handleSearch,
     placeholder, options, hasSearch, searchPlaceHolder, className, defaultValue, hasMultiple } = props
+  const [selectArrayData, setSelectArrayData] = useState(options)
 
-  const handleInputSearch = (event: any) => {
-    handleSearch(event.target.value)
+  const handleChangeSearch = (e: any) => {
+    if (e.trim() === '') setSelectArrayData(options)
+    else {
+      const searchedData = selectArrayData?.filter((emp: any) => emp?.label?.toLowerCase()?.includes(e))
+      setSelectArrayData(searchedData)
+    }
   }
+console.log(selectArrayData);
 
   return (
     <>
@@ -42,17 +50,18 @@ const UserSelector = (props: UserSelectorProps) => {
         dropdownRender={(menu) => (
           <div className='input-wrapper'>
             {hasSearch && <div className='select-search'>
-              <Input
+              {/* <Input
                 prefix={<GlassMagnifier />}
                 placeholder={searchPlaceHolder}
                 className='search-bar'
-                onChange={handleInputSearch} />
+                onChange={handleInputSearch} /> */}
+              <SearchBar placeholder='Search' handleChange={handleChangeSearch} />
             </div>}
             {menu}
           </div>
         )}
       >
-        {options?.map((item: any) => {
+        {selectArrayData?.map((item: any) => {
           const names = item.label.split(" ");
           let initials = "";
           names.forEach((name: any) => {
