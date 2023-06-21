a502235d6fb28787a90a1444a90e73752ab161f5
Muhammad Shoaib <muhammad.shoaib@ceative.co.uk>
2023-06-21 15:58:02 +0500

    counting card bugs fixes

--- src/components/CountingCard/Card.tsx
+++ src/components/CountingCard/Card.tsx
@@ -3,21 +3,26 @@ import { Row } from "antd/es/grid";
 
 interface ICard {
   icon: ReactNode;
-  iconBg:string,
+  iconBg: string;
   title: string;
   count: string | number;
 }
 const Card: FC<ICard> = (props) => {
-  const { icon, title, count,iconBg } = props;
+  const { icon, title, count, iconBg } = props;
   return (
     <div>
       <Row align="middle" className="gap-5">
-        <div style={{ backgroundColor: iconBg }} className="flex items-center justify-center w-[44px] h-[44px] rounded-lg ">
+        <div
+          style={{ backgroundColor: iconBg }}
+          className="flex items-center justify-center w-[44px] h-[44px] rounded-lg "
+        >
           {icon}
         </div>
         <p className="text-base font-semibold">{title}</p>
       </Row>
-      <p className="text-[38px] leading-[46px] font-medium text-primary-color pl-[64px]">{count}</p>
+      <p className="text-[38px] leading-[46px] font-medium text-primary-color pl-[64px]">
+        {+count < 10 ? `0${count}` : count}
+      </p>
     </div>
   );
 };
