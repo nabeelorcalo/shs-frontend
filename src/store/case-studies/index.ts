435d027f6d9c7df5db7e80757ecbb1889ae085a3
Muhammad Shoaib <muhammad.shoaib@ceative.co.uk>
2023-06-21 12:35:24 +0500

    fixes in case studies

--- src/store/case-studies/index.ts
+++ src/store/case-studies/index.ts
@@ -9,8 +9,3 @@ export const caseStudiesTableData = atom({
   key: "caseStudiesTableData",
   default: { count: 0, data: [], pagination: {} },
 });
-
-export const caseStudiesAPICallStatus = atom({
-  key: 'caseStudiesAPICallStatus',
-  default: false
-});
