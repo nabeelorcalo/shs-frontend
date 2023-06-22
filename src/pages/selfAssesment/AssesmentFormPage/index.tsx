e3c9ae31edcd47a2889ee36544cc2dc6bd70f5f5
IhtashamNazir <ihtasham104@gmail.com>
2023-06-22 06:08:47 +0000

    Squashed commit of the following
    commit f0e08c35 
    Author: IhtashamNazir <ihtasham104@gmail.com> 
    Date: Thu Jun 22 2023 10:26:01 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev' into integration/assessment
    
    
    commit a4aa0da1 
    Author: IhtashamNazir <ihtasham104@gmail.com> 
    Date: Thu Jun 22 2023 10:24:23 GMT+0500 (Pakistan Standard Time) 
    
        Assessment actions add edit delete view
    
    
    commit ba2d84dd 
    Author: IhtashamNazir <ihtasham104@gmail.com> 
    Date: Wed Jun 21 2023 08:54:15 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev' into integration/assessment
    
    
    commit 91ee0c1d 
    Author: IhtashamNazir <ihtasham104@gmail.com> 
    Date: Mon Jun 19 2023 16:54:25 GMT+0500 (Pakistan Standard Time) 
    
        base of self assessment work

--- src/pages/selfAssesment/AssesmentFormPage/index.tsx
+++ src/pages/selfAssesment/AssesmentFormPage/index.tsx
@@ -6,25 +6,50 @@ import SignatureAndUploadModal from "../../../components/SignatureAndUploadModal
 import "./style.scss"
 import customCaseStoryHook from "../../../pages/caseStudies/actionHandler";
 import useCustomHook from "../actionHandler"
+import { useRecoilValue } from "recoil"
+import { editOrView, assessmentDataState } from "../../../store"
+import { useNavigate } from "react-router-dom"
+import { ROUTES_CONSTANTS } from "../../../config/constants"
 const mockData = [
   {
     no: 'Technical Skills',
+    objective: 'technical_objective',
+    evidence: 'technical_evidence',
+    objectiveValue: '',
+    evidenceValue: ''
   },
   {
     no: 'Working With Others',
+    objective: 'wwo_objective',
+    evidence: 'wwo_evidence',
+    objectiveValue: '',
+    evidenceValue: ''
   },
   {
     no: 'Self-Management',
+    objective: 'sm_objective',
+    evidence: 'sm_evidence',
+    objectiveValue: '',
+    evidenceValue: ''
   },
   {
     no: 'Commercial Awarenesss',
+    objective: 'ca_objective',
+    evidence: 'ca_evidence',
+    objectiveValue: '',
+    evidenceValue: ''
   },
   {
     no: 'Personal and Professional Development',
+    objective: 'ppd_objective',
+    evidence: 'ppd_evidence',
+    objectiveValue: '',
+    evidenceValue: ''
   },
 ]
 const AssesmentForm = () => {
   const action = useCustomHook();
+  const navigate = useNavigate();
   let {
     uploadFile,
     signPad,
@@ -32,13 +57,18 @@ const AssesmentForm = () => {
     setSignatureText
   } = customCaseStoryHook();
   const [openSignatureModal, setOpenSignatureModal] = useState(false);
+  const [form] = Form.useForm();
   const [disabled, setDisabled] = useState(false);
+  const [status, setStatus] = useState('');
+  const editOrViewData: string = useRecoilValue(editOrView);
+  const assessmentData: any = useRecoilValue(assessmentDataState);
+
   const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({ title: '', internSig: '', internStatus: '', assessmentForm: [{learningCategorie: '', learningObjective: '', evidenceOfProgress: ''}]});
+  const [formData, setFormData] = useState({ title: '', internSig: '', internStatus: '', assessmentForm: []});
 
   // covert base 64 url to file
-  const urlToFile = (url: any) => {
-    let arr = url.split(",");
+  const urlToFile = (url: any) => {    
+    let arr = url?.split(",");
     let mime = arr[0].match(/:(.*?);/)[1];
     let data = arr[1];
     let dataStr = atob(data);
@@ -75,38 +105,35 @@ const AssesmentForm = () => {
     }
   };
 
-
-  const handleChangeForm = (value: string, type: string, data: string) => {
-    console.log('formData', formData);
-    let updatedForm = formData.assessmentForm;
-    let existingObject = false;
-    updatedForm = updatedForm.map((item) => {
-      if (item.learningCategorie === data) {
-        existingObject = true;
-        if (type === 'learningObjective') {
-          return { ...item, learningObjective: value };
-        } else if (type === 'evidenceOfProgress') {
-          return { ...item, evidenceOfProgress: value };
-        }
-      }
-      return item;
-    });
-    if (!existingObject) {
-      const newObject = {
-        learningCategorie: data,
-        learningObjective: type === 'learningObjective' ? value : '',
-        evidenceOfProgress: type === 'evidenceOfProgress' ? value : '',
-      };
-      updatedForm.push(newObject);
-    }
-    setFormData({...formData, assessmentForm: updatedForm});
-    console.log('formData', formData);
+  const draftFunction = async () => {
+    setStatus('Draft');
+    form.submit();
   }
 
-  const addAssessmentHandle = async (draft: boolean) => {
-    let values ={...formData, internStatus: draft ? 'Draft' : 'Submitted'}
+  const backButton = async () => {
+    navigate(`/${ROUTES_CONSTANTS.SELF_ASSESSMENT}`);
+  }
+
+  const addAssessmentHandle = async (values?: any) => {
+    const formValues ={...formData, title: values.title, internStatus: status === 'Draft' ? 'Draft' : 'Submitted'};
+    const categories = ["technical", "wwo", "sm", "ca", "ppd"];
+    let updatedForm: any = [];
+    
+    categories.forEach(category => {
+      const objectiveKey = `${category}_objective`;
+      const evidenceKey = `${category}_evidence`;
+      const idKey = `${category}_id`;
+      const assessmentItem: any = {
+        "learningCategorie": `${category.toUpperCase()}`,
+        "learningObjective": values[objectiveKey],
+        "evidenceOfProgress": values[evidenceKey]
+      };
+      if(editOrViewData === 'edit') assessmentItem['id'] = assessmentData[idKey];
+      updatedForm.push(assessmentItem);
+    });
+    formValues.assessmentForm = updatedForm;
     setLoading(true);
-    await action.saveSelfAssessment(values);
+    editOrViewData === 'edit' ? await action.editSelfAssessment(formValues, assessmentData.assessmentId): await action.saveSelfAssessment(formValues);
     setDisabled(true);
     setLoading(false);
   };
@@ -121,12 +148,13 @@ const AssesmentForm = () => {
     {
       render: (_: any, data: any) => (
         <div className="text_area_wrapper">
-          <Form.Item >
+          <Form.Item name={data.objective} >
             <Input.TextArea 
+              disabled={(editOrViewData && editOrViewData === 'view') ? true : false}
               placeholder="Type here..."
               className="w-full h-[163px] focus:outline-none px-[16px] py-[10px] rounded-lg"
               autoSize={{ minRows: 6, maxRows: 6 }}
-              onChange={ (_, type='learningObjective')=> handleChangeForm(_.target.value, type, data.no)}
+              // onChange={ (_, type='learningObjective')=> handleChangeForm(_.target.value, type, data.no)}
             />
           </Form.Item>
         </div>),
@@ -140,16 +168,17 @@ const AssesmentForm = () => {
     {
       render: (_: any, data: any) => (
         <div className="text_area_wrapper">
-          <Form.Item >
+          <Form.Item name={data.evidence} >
             <Input.TextArea 
+              disabled={(editOrViewData && editOrViewData === 'view') ? true : false}
               placeholder="Type here..."
               className="w-full h-[163px] focus:outline-none px-[16px] py-[10px] rounded-lg"
               autoSize={{ minRows: 6, maxRows: 6 }}
-              onChange={(_, type='evidenceOfProgress')=> handleChangeForm(_.target.value, type, data.no)}
+              // onChange={(_, type='evidenceOfProgress')=> handleChangeForm(_.target.value, type, data.no)}
             />
           </Form.Item>
         </div>),
-      title: <h4>Evidence of Progress  <Tooltip placement="right" color={'#363565'} title={"Give evidence to how your learning objectives are met"}>
+      title: <h4>Evidence of Progress<Tooltip placement="right" color={'#363565'} title={"Give evidence to how your learning objectives are met"}>
         <InTooltipIcon className="ml-5" />
       </Tooltip></h4>,
       width: 500
@@ -162,6 +191,8 @@ const AssesmentForm = () => {
         bordered
         title={<div>Assessment Form | <span className="text-base text-[#363565]">Self Assessment</span></div>}
       />
+
+    <Form form={form} initialValues={editOrViewData ? assessmentData : {}} onFinish={addAssessmentHandle}>
       <BoxWrapper className="selefAssesment_detail_form_wrapper"
         boxShadow=' 0px 0px 8px 1px rgba(9, 161, 218, 0.1)'>
           <Space direction="vertical" size="large">
@@ -173,22 +204,19 @@ const AssesmentForm = () => {
                 {/* <h5 className="report_title ">Report Title</h5> */}
               </Col>
               <Col>
-              <Form name="basic">
                 <Form.Item
                   label="Report Title"
                   name="title"
+                  rules={[{ required: true, message: "Title is required" }]}
                 >
                   <Input 
                     id="01" 
                     placeholder="Enter Title"
                     type="text"  
                     name="title" 
-                    onChange={(e)=>{
-                      // console.log(e.target.value);
-                      setFormData({ ...formData, title: e.target.value});
-                    }} />
+                    disabled={(editOrViewData && editOrViewData === 'view') ? true : false}
+                    />
                 </Form.Item>
-              </Form>
               </Col>
             </Row>
           </Space>
@@ -199,12 +227,28 @@ const AssesmentForm = () => {
           columns={colum}
           tableData={mockData}
         />
+
         <Row gutter={[20, 20]} justify="space-between">
           <Col xs={24} lg={11}>
+          {editOrViewData !== '' && editOrViewData === 'view' ?
+            <div className="">
+              <h4 className="mb-4">{assessmentData.name}</h4>
+              <div className="sign-box w-full rounded-lg flex justify-center items-center">
+                <div className="w-[90%] relative flex items-center justify-center min-h-[120px]">
+                  {action.checkForImage(assessmentData?.internSign) ? (
+                    <img className="absolute w-full h-full overflow-hidden" src={assessmentData?.internSign} />
+                  ) : (
+                    <p>{assessmentData?.internSign}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+          :
             <div className="signature_wraper">
               <h4 className="mb-4">Maria Sanoid</h4>
               <div className="Signatur_modal_opener flex items-center justify-center rounded-lg cursor-pointer" onClick={() => {setOpenSignatureModal(true)}}>Click Here To Sign</div>
             </div>
+          }
           </Col>
           <Col xs={24} lg={11}>
             <div className="signature_wraper">
@@ -214,26 +258,42 @@ const AssesmentForm = () => {
           </Col>
         </Row>
         <div className='flex items-center justify-end form_button_wrapper mt-5'>
-          <Button
-            label="Save Draft"
-            htmlType="button"
-            onClick={() => {addAssessmentHandle(true)}}
-            className="Reset_btn flex items-center justify-center   mr-5"
-          />
-          <Button
-            label="Continue"
-            disabled= {disabled}
-            loading={loading}
-            htmlType="submit"
-            onClick={() => {{addAssessmentHandle(true)}}}
-            className="Apply_btn flex items-center justify-center "
-          />
+          {editOrViewData === 'view' ?
+            <Button
+              label="Back"
+              htmlType="button"
+              onClick={() => backButton()}
+              className="Reset_btn flex items-center justify-center mr-5"
+            />
+            : 
+            <>
+              <Button
+                label= {editOrViewData==="edit"? "Back" : "Cancel"}
+                htmlType="button"
+                onClick={() => backButton()}
+                className="Reset_btn flex items-center justify-center mr-5"
+              />
+              <Button
+                label="Save Draft"
+                htmlType="button"
+                disabled= {disabled}
+                loading={loading}
+                onClick={() => draftFunction()}
+                className="Reset_btn flex items-center justify-center mr-5"
+              /> 
+              <Button
+                label="Continue"
+                disabled= {disabled}
+                loading={loading}
+                htmlType="submit"
+                className="Apply_btn flex items-center justify-center "
+              /> 
+            </>
+          }
         </div>
       </BoxWrapper>
       <SignatureAndUploadModal
         state={openSignatureModal}
-        // okBtnFunc={()=> {HandleSignature()}}
-        // closeFunc={()=> {HandleCleare()}}
         okBtntxt={"Sign"}
         cancelBtntxt={"Cancle"}
         width={650}
@@ -254,6 +314,7 @@ const AssesmentForm = () => {
           </>
         }
       />
+    </Form>
     </div>
   )
 }
--- src/pages/selfAssesment/actionHandler.ts
+++ src/pages/selfAssesment/actionHandler.ts
@@ -1,46 +1,106 @@
-import React from "react";
-import api from "../../api";
-import constants, { ROUTES_CONSTANTS } from "../../config/constants";
-import apiEndpints from "../../config/apiEndpoints";
-import { useNavigate } from 'react-router-dom'
-import { assessmentDataState, filterData, remarkedByData } from "../../store";
-import { useRecoilState } from "recoil";
-import _ from "lodash";
-import { Notifications } from "../../components";
+import api from '../../api';
+import { ROUTES_CONSTANTS } from '../../config/constants';
+import apiEndpints from '../../config/apiEndpoints';
+import { useNavigate } from 'react-router-dom';
+import { debounce } from 'lodash';
+import {
+  assessmentsDataState,
+  assessmentDataState,
+  filterData,
+  remarkedByData,
+} from '../../store';
+import { useRecoilState } from 'recoil';
+import _ from 'lodash';
+import { Notifications } from '../../components';
 
 const useCustomHook = () => {
-  const { ASSESSMENT, MEDIA_UPLOAD } = apiEndpints
+  const { ASSESSMENT, MEDIA_UPLOAD } = apiEndpints;
   const navigate = useNavigate();
-  const [selfAssessment, setSelfAssessment] = useRecoilState(assessmentDataState);
+  const [selfAssessments, setSelfAssessments] =
+    useRecoilState(assessmentsDataState);
+  const [selfAssessment, setSelfAssessment] =
+    useRecoilState(assessmentDataState);
   const [remarkedBy, setRemarkedBy] = useRecoilState(remarkedByData);
   const [filter, setFilter] = useRecoilState(filterData);
 
-  const getSelfAssessment = async () => {
-    const { data } = await api.get(ASSESSMENT.GET_ASSESSMENTS, filter);
-    setSelfAssessment(data);
-    if(_.isEmpty(filter)) {
-      const remarkedObj = _.uniqBy(data, (obj: any)=> obj.remarkedBy)
+  const getSelfAssessments = async (val?: string) => {
+    const hasValue = { search: val } ?? {};
+    const { data } = await api.get(ASSESSMENT.GET_ASSESSMENTS, {
+      ...filter,
+      ...hasValue,
+    });
+    setSelfAssessments(data);
+    if (_.isEmpty(filter)) {
+      const remarkedObj = _.uniqBy(data, (obj: any) => obj.remarkedBy);
       setRemarkedBy(remarkedObj as any);
     }
   };
 
+  const getSelfAssessment = async (id: number) => {
+    const { data } = await api.get(`${ASSESSMENT.GET_ASSESSMENTS}/${id}`);
+    const initialObject: any = {
+      title: data.title,
+      assessmentId: data.id,
+      internSign: data?.internSig,
+      name: `${data?.intern?.userDetail?.firstName} ${data?.intern?.userDetail?.lastName}`,
+    };
+
+    data.assessmentForm.forEach(
+      (item: {
+        learningCategorie: string;
+        learningObjective: any;
+        evidenceOfProgress: any;
+        id: any;
+      }) => {
+        const category = item?.learningCategorie?.toLowerCase() || '';
+        const objectiveKey = `${category}_objective`;
+        const evidenceKey = `${category}_evidence`;
+        const idKey = `${category}_id`;
+        initialObject[objectiveKey] = item?.learningObjective || '';
+        initialObject[evidenceKey] = item?.evidenceOfProgress || '';
+        initialObject[idKey] = item?.id;
+      }
+    );
+    setSelfAssessment(initialObject);
+  };
+
   const saveSelfAssessment = async (assessment: any) => {
-    const asses = _.drop(assessment?.assessmentForm, 1)
-    assessment.assessmentForm = asses;
     const { data } = await api.post(ASSESSMENT.ADD_ASSESSMENT, assessment);
     if (data) {
-      await getSelfAssessment();
-      Notifications({ title: "Success", description: "Assessment added", type: "success" });
-      navigate(`/${ROUTES_CONSTANTS.SELF_ASSESSMENT}`)
+      await getSelfAssessments();
+      Notifications({
+        title: 'Success',
+        description: 'Assessment added',
+        type: 'success',
+      });
+      navigate(`/${ROUTES_CONSTANTS.SELF_ASSESSMENT}`);
     }
   };
 
-  const downloadAssessment = async (assessment: {id: number, downloadType: string}) => {
-    console.log(assessment);
-    
-    const { data } = await api.get(`${ASSESSMENT.GET_ASSESSMENT}/${assessment.id}`, {downloadType: assessment.downloadType});
-    console.log(data);
-    // To be implemented
+  const editSelfAssessment = async (assessment: any, id: number) => {
+    const data = await api.patch(
+      `${ASSESSMENT.EDIT_ASSESSMENT}/${id}`,
+      assessment
+    );
+    if (data) {
+      await getSelfAssessments();
+      Notifications({
+        title: 'Success',
+        description: 'Assessment Edited',
+        type: 'success',
+      });
+      navigate(`/${ROUTES_CONSTANTS.SELF_ASSESSMENT}`);
+    }
+  };
+
+  const downloadAssessment = async (assessment: {
+    id: number;
+    downloadType: string;
+  }) => {
+    const { data } = await api.get(
+      `${ASSESSMENT.GET_ASSESSMENT}/${assessment.id}`,
+      { downloadType: assessment.downloadType }
+    );
   };
 
   const handleFileUpload = async (file: any) => {
@@ -51,27 +111,46 @@ const useCustomHook = () => {
     if (file) {
       formData.append('file', file);
       const fileData = await api.post(MEDIA_UPLOAD, formData, headerConfig);
-      return fileData?.data
-    }
-  }
-
-  const deleteAssessment = async (assessment: {id: number}) => {
-    console.log(assessment);
-    const data = await api.delete(`${ASSESSMENT.DELETE_ASSESSMENT}/${assessment.id}`);
-    if (data.message === 'Success' || data.statusCode === 200) {
-      await getSelfAssessment();
-      Notifications({ title: "Success", description: "Assessment Deleted", type: "success" });
+      return fileData?.data;
     }
   };
-  
+
+  const deleteAssessment = async (assessment: { id: number }) => {
+    const data = await api.delete(
+      `${ASSESSMENT.DELETE_ASSESSMENT}/${assessment.id}`
+    );
+    if (data.message === 'Success' || data.statusCode === 200) {
+      await getSelfAssessments();
+      Notifications({
+        title: 'Success',
+        description: 'Assessment Deleted',
+        type: 'success',
+      });
+    }
+  };
+
+  const checkForImage = (url: string) => {
+    let regex = /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gim;
+    if (url && url.match(regex)) return true;
+    else return false;
+  };
+
+  const debouncedSearch = debounce((value, setSearchName) => {
+    setSearchName(value);
+  }, 500);
+
   return {
-    selfAssessment,
+    selfAssessments,
     remarkedBy,
+    debouncedSearch,
     getSelfAssessment,
+    editSelfAssessment,
+    getSelfAssessments,
     downloadAssessment,
     saveSelfAssessment,
     deleteAssessment,
     handleFileUpload,
+    checkForImage,
   };
 };
 
