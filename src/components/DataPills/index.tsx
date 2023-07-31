54a33e90404df15d3dee5eb3a54373206004e198
Khubaib <khubaib.rafique@ceative.co.uk>
2023-07-31 11:17:14 +0500

    dependents default value wip

--- src/components/DataPills/index.tsx
+++ src/components/DataPills/index.tsx
@@ -1,23 +1,30 @@
-import { PlusOutlined } from "@ant-design/icons"
-import { Input, InputRef, Space, Tag } from "antd"
+import { PlusOutlined } from "@ant-design/icons";
+import { Input, InputRef, Space, Tag } from "antd";
 import { useRef, useState } from "react";
 
 interface Props {
-  name?: string,
-  initialValue?: string[],
-  addInput?: boolean,
-  onNewAddition?: Function
+  name?: string;
+  initialValue?: string[];
+  addInput?: boolean;
+  onNewAddition?: Function;
 }
 
-const DataPill = ({ name = '', initialValue = [], addInput = true, onNewAddition = (list: string[]) => console.log('New Data Added') }: Props) => {
+const DataPill = ({
+  name = "",
+  initialValue = [],
+  addInput = true,
+  onNewAddition = (list: string[]) => console.log("New Data Added"),
+}: Props) => {
   const [inputVisible, setInputVisible] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>('');
+  const [inputValue, setInputValue] = useState<string>("");
   const inputRef = useRef<InputRef>(null);
   const [skillTags, setSkillTags] = useState<string[]>(initialValue);
 
+  console.log(initialValue);
+
   const showInputSkills = () => {
-    setInputVisible(true)
-  }
+    setInputVisible(true);
+  };
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
@@ -28,7 +35,7 @@ const DataPill = ({ name = '', initialValue = [], addInput = true, onNewAddition
       onNewAddition(name, [...skillTags, inputValue]);
     }
     setInputVisible(false);
-    setInputValue('');
+    setInputValue("");
   };
 
   return (
@@ -47,10 +54,14 @@ const DataPill = ({ name = '', initialValue = [], addInput = true, onNewAddition
               autoFocus
               placeholder="New Skill"
               className="w-32 bg-[#e6f4f9]"
-            // style={{ backgroundColor: "#e6f4f9"}}
+              // style={{ backgroundColor: "#e6f4f9"}}
             />
           ) : (
-            <Tag onClick={showInputSkills} color="#e6f4f9" className="py-1 px-4 rounded-3xl">
+            <Tag
+              onClick={showInputSkills}
+              color="#e6f4f9"
+              className="py-1 px-4 rounded-3xl"
+            >
               <span className="text-gray-400">
                 <PlusOutlined className="mr-1" /> Add
               </span>
@@ -58,15 +69,13 @@ const DataPill = ({ name = '', initialValue = [], addInput = true, onNewAddition
           )}
         </>
       ) : null}
-      {skillTags?.map((item: any) =>
+      {skillTags.map((item: any) => (
         <Tag color="#e6f4f9" className="py-1 px-4 rounded-3xl" key={item}>
-          <span className="text-black">
-            {item}
-          </span>
+          <span className="text-black">{item}</span>
         </Tag>
-      )}
+      ))}
     </Space>
-  )
-}
+  );
+};
 
-export default DataPill
+export default DataPill;
