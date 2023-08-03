083de855fc274ada48c4cc92f2ecda444ed1f5f3
Mubashir yusuf <mubashir.yusuf@ceative.co.uk>
2023-08-03 14:24:28 +0500

    department updated in manager module

--- src/pages/internships/actionHandler.ts
+++ src/pages/internships/actionHandler.ts
@@ -57,7 +57,7 @@ const useCustomHook = () => {
   const postNewInternshipsData = async (values: any) => {
     const { title, description, responsibilities, requirements, typeofwork, frequency,
       amount, natureofwork, positions, closingDate, duration, amountType, salaryType,
-      department, status, location ,currencyType} = values
+      department, status, location, currencyType } = values
     const internshipData = {
       "title": title,
       "departmentId": department,
@@ -90,11 +90,12 @@ const useCustomHook = () => {
       title, description, responsibilities,
       requirements, typeofwork, frequency, amount, natureofwork,
       positions, closingDate, duration, internshipType, salaryAmount,
-      department, status, locationId, id } = values
+      department, departmentId, status, locationId, id } = values;
+      
     const internshipData = {
       "id": state?.id ? state?.id : id,
       "title": title,
-      "departmentId": Number(department),
+      "departmentId": departmentId ?? Number(department),
       "description": description,
       "responsibilities": responsibilities,
       "requirements": requirements,
