f98e60e06b8ac5db599d3d826e261ac82eccf73c
Shahzeb Rehman Khattak <shahzebrehman.khattak@ceative.co.uk>
2023-08-09 08:55:15 +0500

    padding added in country code select with nullish operator

--- src/components/CountryCodeSelect/index.tsx
+++ src/components/CountryCodeSelect/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import useCountriesCustomHook from "../../helpers/countriesList";
 import { Avatar, Select, Space } from "antd";
+import './style.scss';
 const { Option } = Select;
 
 const CountryCodeSelect = (props?: any) => {
@@ -14,8 +15,7 @@ const CountryCodeSelect = (props?: any) => {
     getCountriesList();
   }, []);
 
-  const selectCode = allCountriesList
-    ?.filter((a: any) => Object.keys(a.idd).length > 0)
+  const selectCode = allCountriesList?.filter((a: any) => Object?.keys(a.idd).length > 0)
     .map((item: any, index: number) => {
       return {
         key: index,
@@ -26,27 +26,29 @@ const CountryCodeSelect = (props?: any) => {
     });
 
   const listOptions = [
-    ...new Map(selectCode.map((item: any) => [item["label"], item])).values(),
+    ...new Map(selectCode?.map((item: any) => [item["label"], item])).values(),
   ];
 
   return (
-    <Select
-      className="w-full"
-      showSearch={true}
-      onChange={onChange}
-      defaultValue={defaultVal}
-    >
-      {listOptions?.map((item: any) => {
-        return (
-          <Option value={item?.value} key={item.value}>
-            <Space>
-              {item?.avatar && <Avatar size={30} src={item?.avatar}></Avatar>}
-              {item?.label}
-            </Space>
-          </Option>
-        );
-      })}
-    </Select>
+    <div className="country-code-select">
+      <Select
+        className="w-full"
+        showSearch={true}
+        onChange={onChange}
+        defaultValue={defaultVal}
+      >
+        {listOptions?.map((item: any) => {
+          return (
+            <Option value={item?.value} key={item?.value} >
+              <Space>
+                {item?.avatar && <Avatar size={30} src={item?.avatar}></Avatar>}
+                {item?.label}
+              </Space>
+            </Option>
+          );
+        })}
+      </Select>
+    </div>
   );
 };
 
