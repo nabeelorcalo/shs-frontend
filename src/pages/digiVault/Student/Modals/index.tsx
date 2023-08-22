a1576ca11a8d27b225938707919485a47426e8f9
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-08-22 10:42:08 +0500

    #comment some changes

--- src/pages/digiVault/Student/Modals/index.tsx
+++ src/pages/digiVault/Student/Modals/index.tsx
@@ -37,7 +37,7 @@ const DigiVaultModals = (props: any) => {
         Lock
       </span>
 
-      {(studentVault?.lockResponse || studentVault === undefined) ?
+      {((studentVault?.lockResponse || studentVault === undefined) && !state.isModalOpen) ?
         <>
           <Switch onChange={onChange}
             checked={state.isLock}
